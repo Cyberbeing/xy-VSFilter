a5bcea5592f1ecc70f4fcfd7d901d5e039d4dd54
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2011-10-05 21:29:00 +0800

    Merging mpc-hc source rev. 3705.

--- src/subpic/DX9SubPic.h
+++ src/subpic/DX9SubPic.h
@@ -61,30 +61,3 @@ public:
 	STDMETHODIMP ChangeDevice(IUnknown* pDev);
 };
 
-[uuid("CD6D2AA5-20D3-4ebe-A8A9-34D3B00CC253")]
-interface ISubRenderCallback : public IUnknown
-{
-	// NULL means release current device, textures and other resources
-	STDMETHOD(SetDevice)(IDirect3DDevice9 *dev) = 0;
-
-	// destination video rectangle, will be inside (0, 0)-(width, height)
-	// width,height is the size of the entire output window
-	STDMETHOD(Render)(REFERENCE_TIME rtStart,
-		int left, int top, int bottom, int right,
-		int width, int height) = 0;
-};
-
-[uuid("E602585E-C05A-4828-AC69-AF92997F2E0C")]
-interface ISubRenderCallback2 : public ISubRenderCallback
-{
-	STDMETHOD(RenderEx)(REFERENCE_TIME rtStart, REFERENCE_TIME rtStop,
-		REFERENCE_TIME AvgTimePerFrame,
-		int left, int top, int right, int bottom,
-		int width, int height) = 0;
-};
-
-[uuid("9CC7F9F7-3ED1-493c-AF65-527EA1D9947F")]
-interface ISubRender : public IUnknown
-{
-	STDMETHOD(SetCallback)(ISubRenderCallback *cb) = 0;
-};
