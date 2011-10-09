Cyberbeing/xy-VSFilter

commit eaf70dbe6c3756fd57788c9bb40686ddffb6e178

Refactor and add scan_line_data cache which is originally done by cword cache.

src/subtitles/RTS.cpp
@@ -176,7 +176,12 @@ void CWord::DoPaint(const CPoint& psub, const CPoint& trans_org, SharedPtrOverla
     if(iter==overlay_no_blur_cache->hash_end())
     {
         raterize_result.reset(new Overlay());
-        if(!m_fDrawn)
+
+        ScanLineDataCacheKey scan_line_data_key(*this, trans_org);
+        SharedPtrScanLineData scan_line_data;
+        ScanLineDataMruCache* scan_line_data_cache = CacheManager::GetScanLineDataMruCache();
+        ScanLineDataMruCache::hashed_cache_const_iterator iter = scan_line_data_cache->hash_find(scan_line_data_key);
+        if(iter==scan_line_data_cache->hash_end())
         {
             //get outline path, if not cached, create it and cache a copy, else copy from cache
             SharedPtrPathData path_data(new PathData());        
@@ -201,10 +206,11 @@ void CWord::DoPaint(const CPoint& psub, const CPoint& trans_org, SharedPtrOverla
             if(need_transform)
                 Transform(path_data, CPoint(trans_org.x*8, trans_org.y*8));
 
-            if(!ScanConvert(path_data)) return;
+            scan_line_data.reset(new ScanLineData());
+            if(!scan_line_data->ScanConvert(path_data)) return;
             if(m_style.get().borderStyle == 0 && (m_style.get().outlineWidthX+m_style.get().outlineWidthY > 0))
             {
-                if(!CreateWidenedRegion((int)(m_style.get().outlineWidthX+0.5), (int)(m_style.get().outlineWidthY+0.5))) return;
+                if(!scan_line_data->CreateWidenedRegion((int)(m_style.get().outlineWidthX+0.5), (int)(m_style.get().outlineWidthY+0.5))) return;
             }
             else if(m_style.get().borderStyle == 1)
             {
@@ -212,7 +218,11 @@ void CWord::DoPaint(const CPoint& psub, const CPoint& trans_org, SharedPtrOverla
             }
             m_fDrawn = true;      
         }
-        if(!Rasterize(psub.x, psub.y, raterize_result)) return;
+        else
+        {
+            scan_line_data = iter->scan_line_data;
+        }
+        if(!Rasterizer::Rasterize(*scan_line_data, psub.x, psub.y, raterize_result)) return;
 
         OverlayNoBlurMruItem item(overlay_no_blur_key, raterize_result);
         CacheManager::GetOverlayNoBlurMruCache()->update_cache(item);
@@ -221,7 +231,7 @@ void CWord::DoPaint(const CPoint& psub, const CPoint& trans_org, SharedPtrOverla
     {
         raterize_result = iter->overlay;
     }    
-    if(!Blur(*raterize_result, m_style.get().fBlur, m_style.get().fGaussianBlur, *overlay))
+    if(!Rasterizer::Blur(*raterize_result, m_style.get().fBlur, m_style.get().fGaussianBlur, *overlay))
     {
         *overlay = raterize_result;
     }
@@ -1069,13 +1079,13 @@ CRect CLine::PaintShadow(SubPicDesc& spd, CRect& clipRect, BYTE* pAlphaMask, CPo
             CWord::Paint(w, CPoint(x, y), org, &overlay_list);
             if(w->m_style.get().borderStyle == 0)
             {
-                bbox |= w->Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw,
+                bbox |= Rasterizer::Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw,
                                 w->m_ktype > 0 || w->m_style.get().alpha[0] < 0xff,
                                 (w->m_style.get().outlineWidthX+w->m_style.get().outlineWidthY > 0) && !(w->m_ktype == 2 && time < w->m_kstart));
             }
             else if(w->m_style.get().borderStyle == 1 && w->m_pOpaqueBox)
             {
-                bbox |= w->m_pOpaqueBox->Draw(spd, overlay_list.next->overlay, clipRect, pAlphaMask, x, y, sw, true, false);
+                bbox |= Rasterizer::Draw(spd, overlay_list.next->overlay, clipRect, pAlphaMask, x, y, sw, true, false);
             }
         }
         p.x += w->m_width;
@@ -1112,11 +1122,11 @@ CRect CLine::PaintOutline(SubPicDesc& spd, CRect& clipRect, BYTE* pAlphaMask, CP
             CWord::Paint(w, CPoint(x, y), org, &overlay_list);
             if(w->m_style.get().borderStyle == 0)
             {
-                bbox |= w->Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw, !w->m_style.get().alpha[0] && !w->m_style.get().alpha[1] && !alpha, true);
+                bbox |= Rasterizer::Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw, !w->m_style.get().alpha[0] && !w->m_style.get().alpha[1] && !alpha, true);
             }
             else if(w->m_style.get().borderStyle == 1 && w->m_pOpaqueBox)
             {
-                bbox |= w->m_pOpaqueBox->Draw(spd, overlay_list.next->overlay, clipRect, pAlphaMask, x, y, sw, true, false);
+                bbox |= Rasterizer::Draw(spd, overlay_list.next->overlay, clipRect, pAlphaMask, x, y, sw, true, false);
             }
         }
         p.x += w->m_width;
@@ -1187,7 +1197,7 @@ CRect CLine::PaintBody(SubPicDesc& spd, CRect& clipRect, BYTE* pAlphaMask, CPoin
         }
         OverlayList overlay_list;
         CWord::Paint(w, CPoint(x, y), org, &overlay_list);
-        bbox |= w->Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw, true, false);
+        bbox |= Rasterizer::Draw(spd, overlay_list.overlay, clipRect, pAlphaMask, x, y, sw, true, false);
         p.x += w->m_width;
     }
     return(bbox);
@@ -1653,8 +1663,9 @@ void CRenderedTextSubtitle::Deinit()
     m_size = CSize(0, 0);
     m_vidrect.SetRectEmpty();
 
-    CacheManager::GetPathDataMruCache()->clear();
     CacheManager::GetCWordMruCache()->clear();
+    CacheManager::GetPathDataMruCache()->clear();
+    CacheManager::GetScanLineDataMruCache()->clear();
     CacheManager::GetOverlayNoBlurMruCache()->clear();
     CacheManager::GetOverlayMruCache()->clear();
 }

src/filters/transform/vsfilter/DirectVobSub.cpp
@@ -542,31 +542,6 @@ STDMETHODIMP CDirectVobSub::put_OverlayNoBlurCacheMaxItemNum(int overlay_no_blur
     return S_OK;
 }
 
-STDMETHODIMP CDirectVobSub::get_CachesInfo(CachesInfo* caches_info)
-{
-    CAutoLock cAutoLock(&m_propsLock);
-    if(caches_info)
-    {
-        caches_info->path_cache_cur_item_num    = 0;
-        caches_info->path_cache_hit_count       = 0;
-        caches_info->path_cache_query_count     = 0;
-        caches_info->scanline_cache_cur_item_num= 0;
-        caches_info->scanline_cache_hit_count   = 0;
-        caches_info->scanline_cache_query_count = 0;
-        caches_info->non_blur_cache_cur_item_num= 0;
-        caches_info->non_blur_cache_hit_count   = 0;
-        caches_info->non_blur_cache_query_count = 0;
-        caches_info->overlay_cache_cur_item_num = 0;
-        caches_info->overlay_cache_hit_count    = 0;
-        caches_info->overlay_cache_query_count  = 0;
-        return S_OK;
-    }
-    else 
-    {
-        return S_FALSE;
-    }
-}
-
 STDMETHODIMP CDirectVobSub::get_SubpixelPositionLevel(int* subpixel_pos_level)
 {
     CAutoLock cAutoLock(&m_propsLock);

src/filters/transform/vsfilter/DirectVobSub.h
@@ -114,8 +114,6 @@ public:
     STDMETHODIMP get_OverlayNoBlurCacheMaxItemNum(int* overlay_no_blur_cache_max_item_num);
     STDMETHODIMP put_OverlayNoBlurCacheMaxItemNum(int overlay_no_blur_cache_max_item_num);
 
-    STDMETHODIMP get_CachesInfo(CachesInfo* caches_info);
-
     STDMETHODIMP get_SubpixelPositionLevel(int* subpixel_pos_level);
     STDMETHODIMP put_SubpixelPositionLevel(int subpixel_pos_level);
 

src/filters/transform/vsfilter/DirectVobSubFilter.cpp
@@ -1120,26 +1120,6 @@ STDMETHODIMP CDirectVobSubFilter::put_OverlayNoBlurCacheMaxItemNum(int overlay_n
     return hr;
 }
 
-STDMETHODIMP CDirectVobSubFilter::get_CachesInfo(CachesInfo* caches_info)
-{
-    CAutoLock cAutoLock(&m_csQueueLock);
-    HRESULT hr = CDirectVobSub::get_CachesInfo(caches_info);
-
-    caches_info->path_cache_cur_item_num    = CacheManager::GetPathDataMruCache()->get_cur_items_num();
-    caches_info->path_cache_hit_count       = CacheManager::GetPathDataMruCache()->get_cache_hit();
-    caches_info->path_cache_query_count     = CacheManager::GetPathDataMruCache()->get_query_count();
-    caches_info->scanline_cache_cur_item_num= CacheManager::GetCWordMruCache()->get_cur_items_num();
-    caches_info->scanline_cache_hit_count   = CacheManager::GetCWordMruCache()->get_cache_hit();
-    caches_info->scanline_cache_query_count = CacheManager::GetCWordMruCache()->get_query_count();
-    caches_info->non_blur_cache_cur_item_num= CacheManager::GetOverlayNoBlurMruCache()->get_cur_items_num();
-    caches_info->non_blur_cache_hit_count   = CacheManager::GetOverlayNoBlurMruCache()->get_cache_hit();
-    caches_info->non_blur_cache_query_count = CacheManager::GetOverlayNoBlurMruCache()->get_query_count();
-    caches_info->overlay_cache_cur_item_num = CacheManager::GetOverlayMruCache()->get_cur_items_num();
-    caches_info->overlay_cache_hit_count    = CacheManager::GetOverlayMruCache()->get_cache_hit();
-    caches_info->overlay_cache_query_count  = CacheManager::GetOverlayMruCache()->get_query_count();
-    return hr;
-}
-
 STDMETHODIMP CDirectVobSubFilter::put_SubpixelPositionLevel(int subpixel_pos_level)
 {
     CAutoLock cAutolock(&m_csQueueLock);

src/filters/transform/vsfilter/DirectVobSubFilter.h
@@ -103,9 +103,8 @@ public:
     STDMETHODIMP put_PathDataCacheMaxItemNum(int path_data_cache_max_item_num);
     STDMETHODIMP put_OverlayNoBlurCacheMaxItemNum(int overlay_no_blur_cache_max_item_num);
 
-    STDMETHODIMP get_CachesInfo(CachesInfo* caches_info);
-
     STDMETHODIMP put_SubpixelPositionLevel(int subpixel_pos_level);
+
 
     STDMETHODIMP get_MediaFPS(bool* fEnabled, double* fps);
     STDMETHODIMP put_MediaFPS(bool fEnabled, double fps);

src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
@@ -779,47 +779,6 @@ CDVSBasePPage(NAME("DirectVobSub More Property Page"), pUnk, IDD_DVSMOREPAGE, ID
 
 bool CDVSMorePPage::OnMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch(uMsg)
-    {
-    case WM_COMMAND:
-        {
-            switch(HIWORD(wParam))
-            {
-            case BN_CLICKED:
-                {
-                    if(LOWORD(wParam) == IDC_CACHES_INFO_BTN)
-                    {
-                        AFX_MANAGE_STATE(AfxGetStaticModuleState());
-
-                        IDirectVobSub::CachesInfo caches_info;
-                        m_pDirectVobSub->get_CachesInfo(&caches_info);
-                        CString msg;
-                        msg.Format(_T("Cache LV 4(path cache)\n")\
-                                   _T("\t:%ld/%ld/%ld(stored num/hit_count/query_count)\n")\
-                                   _T("Cache LV 3(scanline cache)\n")\
-                                   _T("\t:%ld/%ld/%ld(stored num/hit_count/query_count)\n")\
-                                   _T("Cache LV 2(non-blur cache)\n")\
-                                   _T("\t:%ld/%ld/%ld(stored num/hit_count/query_count)\n")\
-                                   _T("Cache LV 1(overlay cache)\n")\
-                                   _T("\t:%ld/%ld/%ld(stored num/hit_count/query_count)"),
-                            caches_info.path_cache_cur_item_num,     caches_info.path_cache_hit_count,     caches_info.path_cache_query_count,
-                            caches_info.scanline_cache_cur_item_num, caches_info.scanline_cache_hit_count, caches_info.scanline_cache_query_count,
-                            caches_info.non_blur_cache_cur_item_num, caches_info.non_blur_cache_hit_count, caches_info.non_blur_cache_query_count,
-                            caches_info.overlay_cache_cur_item_num,  caches_info.overlay_cache_hit_count,  caches_info.overlay_cache_query_count);
-                        MessageBox(
-                            m_hwnd,
-                            msg,
-                            _T("Caches Info"),
-                            MB_OK | MB_ICONINFORMATION | MB_APPLMODAL
-                            );
-                        return(true);
-                    }
-                }
-                break;
-            }
-        }
-        break;
-    }
     return(false);
 }
 

src/filters/transform/vsfilter/IDirectVobSub.h
@@ -287,18 +287,6 @@ extern "C" {
             int overlay_no_blur_cache_max_item_num
             ) PURE;
 
-        struct CachesInfo
-        {
-            std::size_t path_cache_cur_item_num, path_cache_query_count,path_cache_hit_count,
-                scanline_cache_cur_item_num, scanline_cache_query_count,scanline_cache_hit_count,
-                non_blur_cache_cur_item_num, non_blur_cache_query_count,non_blur_cache_hit_count,
-                overlay_cache_cur_item_num, overlay_cache_query_count,overlay_cache_hit_count;
-        };
-        STDMETHOD(get_CachesInfo) (THIS_
-            CachesInfo* cache_info
-            ) PURE;
-
-
         STDMETHOD(get_SubpixelPositionLevel) (THIS_
             int* subpixel_pos_level
             ) PURE;

src/subtitles/RTS.h
@@ -64,7 +64,7 @@ class CWord;
 typedef CWord* PCWord;
 typedef ::boost::shared_ptr<CWord> SharedPtrCWord;
 typedef ::boost::shared_ptr<CPolygon> SharedPtrCPolygon;
-class CWord : public Rasterizer, public ::boost::enable_shared_from_this<CWord>
+class CWord : public ::boost::enable_shared_from_this<CWord>
 {
     bool m_fDrawn;
     CPoint m_p;

commit 71c6d097fe61cbbf69c793319f7e4a2d6ba4b81d

Fixed an cache issue introduced in Commit:eaf70dbe6c3756fd57788c9bb40686ddffb6e178.

src/subtitles/RTS.cpp
@@ -216,6 +216,8 @@ void CWord::DoPaint(const CPoint& psub, const CPoint& trans_org, SharedPtrOverla
             {
                 if(!CreateOpaqueBox()) return;
             }
+            ScanLineDataMruItem item(scan_line_data_key, scan_line_data);
+            scan_line_data_cache->update_cache(item);
             m_fDrawn = true;      
         }
         else