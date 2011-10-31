Cyberbeing/xy-VSFilter

commit c9ab35c1118f5aabc60fbb29f0e07ea0d71b24c8

Refactor ass tag parsing pipeline, prepare for cache.

src/subtitles/RTS.cpp
@@ -2108,7 +2108,9 @@ bool CRenderedTextSubtitle::ParseSSATag( CAtlList<AssTag> *assTags, const CStrin
         case CMD_move:
         case CMD_org:
         case CMD_pos:
+            break;
         case CMD_t:
+            ParseSSATag(&assTag.embeded, params[0]);
             break;
         case CMD_COUNT:
             nUnrecognizedTags++;
@@ -2123,13 +2125,10 @@ bool CRenderedTextSubtitle::ParseSSATag( CAtlList<AssTag> *assTags, const CStrin
     return(true);
 }
 
-bool CRenderedTextSubtitle::ParseSSATag(CSubtitle* sub, const CStringW& str, STSStyle& style, const STSStyle& org, bool fAnimate)
+bool CRenderedTextSubtitle::ParseSSATag( CSubtitle* sub, const CAtlList<AssTag>& assTags, STSStyle& style, const STSStyle& org, bool fAnimate /*= false*/ )
 {
     if(!sub) return(false);
-
-    CAtlList<AssTag> assTags;
-    ParseSSATag(&assTags, str);
-
+    
     POSITION pos = assTags.GetHeadPosition();
     while(pos)
     {
@@ -2573,7 +2572,7 @@ bool CRenderedTextSubtitle::ParseSSATag(CSubtitle* sub, const CStringW& str, STS
                     m_animAccel = wcstod(params[2], NULL);
                     param = params[3];
                 }
-                ParseSSATag(sub, param, style, org, true);
+                ParseSSATag(sub, assTag.embeded, style, org, true);
                 sub->m_fAnimated = true;
                 break;
             }
@@ -2626,6 +2625,14 @@ bool CRenderedTextSubtitle::ParseSSATag(CSubtitle* sub, const CStringW& str, STS
         }
     }
     return(true);
+}
+
+bool CRenderedTextSubtitle::ParseSSATag(CSubtitle* sub, const CStringW& str, STSStyle& style, const STSStyle& org, bool fAnimate)
+{
+    if(!sub) return(false);
+    CAtlList<AssTag> assTags;
+    ParseSSATag(&assTags, str);
+    return ParseSSATag(sub, assTags, style, org, fAnimate);
 }
 
 bool CRenderedTextSubtitle::ParseHtmlTag(CSubtitle* sub, CStringW str, STSStyle& style, STSStyle& org)

src/subtitles/RTS.h
@@ -339,6 +339,7 @@ class CRenderedTextSubtitle : public CSubPicProviderImpl, public ISubStream, pub
         CStringW cmd;
         AssCmdType cmdType;
         CAtlArray<CStringW> strParams;
+        CAtlList<AssTag> embeded;
     };
 
     // temp variables, used when parsing the script
@@ -354,7 +355,9 @@ class CRenderedTextSubtitle : public CSubPicProviderImpl, public ISubStream, pub
     void ParseString(CSubtitle* sub, CStringW str, const FwSTSStyle& style);
     void ParsePolygon(CSubtitle* sub, const CStringW& str, const FwSTSStyle& style);
     static bool ParseSSATag(CAtlList<AssTag> *assTags, const CStringW& str);
+    bool ParseSSATag(CSubtitle* sub, const CAtlList<AssTag>& assTags, STSStyle& style, const STSStyle& org, bool fAnimate = false);
     bool ParseSSATag(CSubtitle* sub, const CStringW& str, STSStyle& style, const STSStyle& org, bool fAnimate = false);
+
     bool ParseHtmlTag(CSubtitle* sub, CStringW str, STSStyle& style, STSStyle& org);
 
     double CalcAnimation(double dst, double src, bool fAnimate);

commit 652c2807387d524ed8faa1749f80049fb7b50926

Fix \t tags not working bug, issue 25, introduced in Commit c9ab35c1118f5aabc60fbb29f0e07ea0d71b24c8.

src/subtitles/RTS.cpp
@@ -2175,7 +2175,7 @@ bool CRenderedTextSubtitle::ParseSSATag( AssTagList *assTags, const CStringW& st
         case CMD_pos:
             break;
         case CMD_t:
-            ParseSSATag(&assTag.embeded, params[0]);
+            ParseSSATag(&assTag.embeded, params[params.GetCount()-1]);
             break;
         case CMD_COUNT:
             nUnrecognizedTags++;