a6e24e3e140397784ac39355868f2e5800a3399f
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2011-12-18 16:15:55 +0800

    Add "LAV splitter"/"MPC-HC MPEG PS/TS/PVA source" into search list of auto-loading function.

--- src/filters/transform/vsfilter/DirectVobSubFilter.cpp
+++ src/filters/transform/vsfilter/DirectVobSubFilter.cpp
@@ -1710,7 +1710,10 @@ bool CDirectVobSubFilter2::ShouldWeAutoload(IFilterGraph* pGraph)
 		|| (pBF = FindFilter(L"{803E8280-F3CE-4201-982C-8CD8FB512004}", pGraph)) // dsm source
 		|| (pBF = FindFilter(L"{0912B4DD-A30A-4568-B590-7179EBB420EC}", pGraph)) // dsm splitter
 		|| (pBF = FindFilter(L"{3CCC052E-BDEE-408a-BEA7-90914EF2964B}", pGraph)) // mp4 source
-		|| (pBF = FindFilter(L"{61F47056-E400-43d3-AF1E-AB7DFFD4C4AD}", pGraph))) // mp4 splitter
+		|| (pBF = FindFilter(L"{61F47056-E400-43d3-AF1E-AB7DFFD4C4AD}", pGraph)) // mp4 splitter
+        || (pBF = FindFilter(L"{171252A0-8820-4AFE-9DF8-5C92B2D66B04}", pGraph)) // LAV splitter
+        || (pBF = FindFilter(L"{1365BE7A-C86A-473C-9A41-C0A6E82C9FA3}", pGraph)) // MPC-HC MPEG PS/TS/PVA source
+        ) 
 		{
 			BeginEnumPins(pBF, pEP, pPin)
 			{
