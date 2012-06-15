Cyberbeing/xy-VSFilter

commit 2ecdcd9a14a76b013a024a7722531254bf48f0da

Add outline path cache.

src/subtitles/Rasterizer.cpp
@@ -1529,6 +1529,41 @@ PathData::PathData():mpPathTypes(NULL), mpPathPoints(NULL), mPathPoints(0)
 {
 }
 
+PathData::PathData( const PathData& src )
+{
+    //TODO: deal with the case that src.mPathPoints<0 
+    if(mPathPoints!=src.mPathPoints && src.mPathPoints>0)
+    {
+        mPathPoints = src.mPathPoints;
+        mpPathTypes = (BYTE*)realloc(mpPathTypes, mPathPoints * sizeof(BYTE));
+        mpPathPoints = (POINT*)realloc(mpPathPoints, mPathPoints * sizeof(POINT));
+    }
+    if(src.mPathPoints>0)
+    {
+        memcpy(mpPathTypes, src.mpPathTypes, mPathPoints*sizeof(BYTE));
+        memcpy(mpPathPoints, src.mpPathPoints, mPathPoints*sizeof(POINT));
+    }
+}
+
+const PathData& PathData::operator=( const PathData& src )
+{
+    if(this!=&src)
+    {
+        if(mPathPoints!=src.mPathPoints && src.mPathPoints>0)
+        {
+            mPathPoints = src.mPathPoints;
+            mpPathTypes = (BYTE*)realloc(mpPathTypes, mPathPoints * sizeof(BYTE));
+            mpPathPoints = (POINT*)realloc(mpPathPoints, mPathPoints * sizeof(POINT));
+        }
+        if(src.mPathPoints>0)
+        {
+            memcpy(mpPathTypes, src.mpPathTypes, mPathPoints*sizeof(BYTE));
+            memcpy(mpPathPoints, src.mpPathPoints, mPathPoints*sizeof(POINT));
+        }
+    }
+    return *this;
+}
+
 PathData::~PathData()
 {
     _TrashPath();

src/filters/transform/vsfilter/DirectVobSub.cpp
@@ -55,9 +55,12 @@ CDirectVobSub::CDirectVobSub()
     m_overlay_cache_max_item_num = theApp.GetProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_OVERLAY_CACHE_MAX_ITEM_NUM), 256);
     if(m_overlay_cache_max_item_num<0) m_overlay_cache_max_item_num = 0;
     
-    m_word_cache_max_item_num = theApp.GetProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_WORD_CAHCHE_MAX_ITEM_NUM), 512);
+    m_word_cache_max_item_num = theApp.GetProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_WORD_CACHE_MAX_ITEM_NUM), 512);
 	if(m_word_cache_max_item_num<0) m_word_cache_max_item_num = 0;
     
+    m_path_data_cache_max_item_num = theApp.GetProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_PATH_DATA_CACHE_MAX_ITEM_NUM), 512);
+    if(m_path_data_cache_max_item_num<0) m_path_data_cache_max_item_num = 0;
+
     m_subpixel_pos_level = theApp.GetProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_SUBPIXEL_POS_LEVEL), SubpixelPositionControler::FOUR_X_FOUR);
     if(m_subpixel_pos_level<0) m_subpixel_pos_level=0;
     else if(m_subpixel_pos_level>=SubpixelPositionControler::MAX_COUNT) m_subpixel_pos_level=SubpixelPositionControler::EIGHT_X_EIGHT;
@@ -490,6 +493,25 @@ STDMETHODIMP CDirectVobSub::put_CWordCacheMaxItemNum(int word_cache_max_item_num
     return S_OK;
 }
 
+STDMETHODIMP CDirectVobSub::get_PathDataCacheMaxItemNum(int* path_data_cache_max_item_num)
+{
+    CAutoLock cAutoLock(&m_propsLock);
+
+    if(path_data_cache_max_item_num) *path_data_cache_max_item_num = m_path_data_cache_max_item_num;
+
+    return S_OK;
+}
+
+STDMETHODIMP CDirectVobSub::put_PathDataCacheMaxItemNum(int path_data_cache_max_item_num)
+{
+    CAutoLock cAutoLock(&m_propsLock);
+
+    if(m_path_data_cache_max_item_num == path_data_cache_max_item_num || path_data_cache_max_item_num<0) return S_FALSE;
+    m_path_data_cache_max_item_num = path_data_cache_max_item_num;
+
+    return S_OK;
+}
+
 STDMETHODIMP CDirectVobSub::get_SubpixelPositionLevel(int* subpixel_pos_level)
 {
     CAutoLock cAutoLock(&m_propsLock);
@@ -538,7 +560,8 @@ STDMETHODIMP CDirectVobSub::UpdateRegistry()
 	theApp.WriteProfileInt(ResStr(IDS_R_TEXT), ResStr(IDS_RT_AUTOPARCOMPENSATION), m_ePARCompensationType);
 
     theApp.WriteProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_OVERLAY_CACHE_MAX_ITEM_NUM), m_overlay_cache_max_item_num);
-    theApp.WriteProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_WORD_CAHCHE_MAX_ITEM_NUM), m_word_cache_max_item_num);
+    theApp.WriteProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_WORD_CACHE_MAX_ITEM_NUM), m_word_cache_max_item_num);
+    theApp.WriteProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_PATH_DATA_CACHE_MAX_ITEM_NUM), m_path_data_cache_max_item_num);
     theApp.WriteProfileInt(ResStr(IDS_R_PERFORMANCE), ResStr(IDS_RP_SUBPIXEL_POS_LEVEL), m_subpixel_pos_level);
 	return S_OK;
 }

src/filters/transform/vsfilter/DirectVobSub.h
@@ -45,6 +45,7 @@ protected:
 
     int m_overlay_cache_max_item_num;
     int m_word_cache_max_item_num;
+    int m_path_data_cache_max_item_num;
     int m_subpixel_pos_level;
 
 	STSStyle m_defStyle;
@@ -104,6 +105,9 @@ public:
 
     STDMETHODIMP get_CWordCacheMaxItemNum(int* word_cache_max_item_num);
     STDMETHODIMP put_CWordCacheMaxItemNum(int word_cache_max_item_num);
+
+    STDMETHODIMP get_PathDataCacheMaxItemNum(int* path_data_cache_max_item_num);
+    STDMETHODIMP put_PathDataCacheMaxItemNum(int path_data_cache_max_item_num);
 
     STDMETHODIMP get_SubpixelPositionLevel(int* subpixel_pos_level);
     STDMETHODIMP put_SubpixelPositionLevel(int subpixel_pos_level);

src/filters/transform/vsfilter/DirectVobSubFilter.cpp
@@ -1086,6 +1086,19 @@ STDMETHODIMP CDirectVobSubFilter::put_CWordCacheMaxItemNum( int word_cache_max_i
     return hr;
 }
 
+STDMETHODIMP CDirectVobSubFilter::put_PathDataCacheMaxItemNum(int path_data_cache_max_item_num)
+{
+    CAutoLock cAutolock(&m_csQueueLock);
+    HRESULT hr = CDirectVobSub::put_PathDataCacheMaxItemNum(path_data_cache_max_item_num);
+
+    if(hr == NOERROR)
+    {
+        CacheManager::GetPathDataMruCache()->set_max_num_items(m_path_data_cache_max_item_num);
+    }
+
+    return hr;
+}
+
 STDMETHODIMP CDirectVobSubFilter::put_SubpixelPositionLevel(int subpixel_pos_level)
 {
     CAutoLock cAutolock(&m_csQueueLock);

src/filters/transform/vsfilter/DirectVobSubFilter.h
@@ -100,6 +100,7 @@ public:
 
     STDMETHODIMP put_OverlayCacheMaxItemNum(int overlay_cache_max_item_num);
     STDMETHODIMP put_CWordCacheMaxItemNum(int word_cache_max_item_num);
+    STDMETHODIMP put_PathDataCacheMaxItemNum(int path_data_cache_max_item_num);
     STDMETHODIMP put_SubpixelPositionLevel(int subpixel_pos_level);
 
 

src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
@@ -771,6 +771,7 @@ CDVSMorePPage::CDVSMorePPage(LPUNKNOWN pUnk, HRESULT* phr) :
 CDVSBasePPage(NAME("DirectVobSub More Property Page"), pUnk, IDD_DVSMOREPAGE, IDD_DVSMOREPAGE)
 {
     BindControl(IDC_SPINPathCache, m_path_cache);
+    BindControl(IDC_SPINScanlineCache, m_scanline_cache);
     BindControl(IDC_SPINOverlayCache, m_overlay_cache);
     BindControl(IDC_COMBO_SUBPIXEL_POS, m_combo_subpixel_pos);
 }
@@ -786,12 +787,14 @@ void CDVSMorePPage::UpdateObjectData(bool fSave)
     {
         m_pDirectVobSub->put_OverlayCacheMaxItemNum(m_overlay_cache_max_item_num);
         m_pDirectVobSub->put_CWordCacheMaxItemNum(m_word_cache_max_item_num);
+        m_pDirectVobSub->put_PathDataCacheMaxItemNum(m_path_cache_max_item_num);
         m_pDirectVobSub->put_SubpixelPositionLevel(m_subpixel_pos_level);
     }
     else
     {
         m_pDirectVobSub->get_OverlayCacheMaxItemNum(&m_overlay_cache_max_item_num);
         m_pDirectVobSub->get_CWordCacheMaxItemNum(&m_word_cache_max_item_num);
+        m_pDirectVobSub->get_PathDataCacheMaxItemNum(&m_path_cache_max_item_num);
         m_pDirectVobSub->get_SubpixelPositionLevel(&m_subpixel_pos_level);
     }
 }
@@ -800,8 +803,9 @@ void CDVSMorePPage::UpdateControlData(bool fSave)
 {
     if(fSave)
     {
-        m_overlay_cache_max_item_num = m_overlay_cache.GetPos32();
-        m_word_cache_max_item_num = m_path_cache.GetPos32();
+        m_overlay_cache_max_item_num = m_overlay_cache.GetPos32();        
+        m_word_cache_max_item_num = m_scanline_cache.GetPos32();
+        m_path_cache_max_item_num = m_path_cache.GetPos32();
         if (m_combo_subpixel_pos.GetCurSel() != CB_ERR)
         {
             m_subpixel_pos_level = m_combo_subpixel_pos.GetCurSel();
@@ -814,10 +818,13 @@ void CDVSMorePPage::UpdateControlData(bool fSave)
     else
     {
         m_overlay_cache.SetRange32(0, 2048);
+        m_scanline_cache.SetRange32(0, 2048);
         m_path_cache.SetRange32(0, 2048);
 
         m_overlay_cache.SetPos32(m_overlay_cache_max_item_num);
-        m_path_cache.SetPos32(m_word_cache_max_item_num);
+        m_scanline_cache.SetPos32(m_word_cache_max_item_num);
+        m_path_cache.SetPos32(m_path_cache_max_item_num);
+
         int temp = m_subpixel_pos_level;
         if(m_subpixel_pos_level < 0)
             temp = 0;

src/filters/transform/vsfilter/DirectVobSubPropPage.h
@@ -153,9 +153,9 @@ public:
 [uuid("69CE757B-E8C0-4B0A-9EA0-CEA284096F98")]
 class CDVSMorePPage : public CDVSBasePPage
 {
-    int m_overlay_cache_max_item_num, m_word_cache_max_item_num, m_subpixel_pos_level;
+    int m_overlay_cache_max_item_num, m_path_cache_max_item_num, m_word_cache_max_item_num, m_subpixel_pos_level;
 
-    CSpinButtonCtrl m_path_cache, m_overlay_cache;
+    CSpinButtonCtrl m_path_cache, m_scanline_cache, m_overlay_cache;
     CComboBox m_combo_subpixel_pos;
 protected:
     virtual bool OnMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);

src/filters/transform/vsfilter/IDirectVobSub.h
@@ -271,6 +271,14 @@ extern "C" {
             int word_cache_max_item_num
             ) PURE;
 
+        STDMETHOD(get_PathDataCacheMaxItemNum) (THIS_
+            int* path_data_cache_max_item_num
+            ) PURE;
+
+        STDMETHOD(put_PathDataCacheMaxItemNum) (THIS_
+            int path_data_cache_max_item_num
+            ) PURE;
+
         STDMETHOD(get_SubpixelPositionLevel) (THIS_
             int* subpixel_pos_level
             ) PURE;

commit 2b00bf90c54ec89f014ebfdd1fa9ad17e8c0db7d

Fix an error introduced in Commit:2ecdcd9a14a76b013a024a7722531254bf48f0da and not totally fix in Commit:25bd58a533fda08e8da978fbf2ca487034c0e02c, although it would not be triggered currently.

src/subtitles/Rasterizer.cpp
@@ -1863,7 +1863,7 @@ PathData::PathData():mpPathTypes(NULL), mpPathPoints(NULL), mPathPoints(0)
 {
 }
 
-PathData::PathData( const PathData& src ):mPathPoints(src.mPathPoints)
+PathData::PathData( const PathData& src ):mpPathTypes(NULL), mpPathPoints(NULL), mPathPoints(src.mPathPoints)
 {
     //TODO: deal with the case that src.mPathPoints<0 
     if(mPathPoints>0)