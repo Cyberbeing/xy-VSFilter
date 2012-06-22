38337dadace555776e2b670d02e1a075d9018e44
xy <yuzhuohuang@qq.com>
2012-06-22 15:42:27 +0800

    Move dirty rects merging code.

---
+++ src/dsutil/xy_utils.h
@@ -0,0 +1,10 @@
+#ifndef __XY_UTILS_03D97DFD_68D1_4760_8F70_8AB38638E015_H__
+#define __XY_UTILS_03D97DFD_68D1_4760_8F70_8AB38638E015_H__
+
+#include <atlcoll.h>
+#include <atltypes.h>
+
+void MergeRects(const CAtlList<CRect>& input, CAtlList<CRect>* output);
+
+
+#endif // __XY_UTILS_03D97DFD_68D1_4760_8F70_8AB38638E015_H__
