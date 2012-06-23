Cyberbeing/xy-VSFilter

commit 59d9decce762a384c815000ecd74911fea1b82e7

Save one copy for \clip.

src/subtitles/Rasterizer.cpp
@@ -662,10 +662,24 @@ bool Rasterizer::Rasterize(const ScanLineData2& scan_line_data2, int xsub, int y
     overlay->mOverlayHeight = ((height+7)>>3) + 1;
     overlay->mOverlayPitch = (overlay->mOverlayWidth+15)&~15;
 
-    overlay->mpOverlayBuffer.base = (byte*)xy_malloc(2 * overlay->mOverlayPitch * overlay->mOverlayHeight);
-    memset(overlay->mpOverlayBuffer.base, 0, 2 * overlay->mOverlayPitch * overlay->mOverlayHeight);
-    overlay->mpOverlayBuffer.body = overlay->mpOverlayBuffer.base;
-    overlay->mpOverlayBuffer.border = overlay->mpOverlayBuffer.base + overlay->mOverlayPitch * overlay->mOverlayHeight;        
+    BYTE* body = reinterpret_cast<BYTE*>(xy_malloc(overlay->mOverlayPitch * overlay->mOverlayHeight));
+    if( body==NULL )
+    {
+        return false;
+    }
+    overlay->mBody.reset(body, xy_free);
+    memset(body, 0, overlay->mOverlayPitch * overlay->mOverlayHeight);
+    BYTE* border = NULL;
+    if (!overlay->mfWideOutlineEmpty)
+    {
+        border = reinterpret_cast<BYTE*>(xy_malloc(overlay->mOverlayPitch * overlay->mOverlayHeight));
+        if (border==NULL)
+        {
+            return false;
+        }
+        overlay->mBorder.reset(border, xy_free);
+        memset(border, 0, overlay->mOverlayPitch * overlay->mOverlayHeight);
+    }
 
     // Are we doing a border?
     const ScanLineData::tSpanBuffer* pOutline[2] = {&(scan_line_data.mOutline), &(scan_line_data2.mWideOutline)};
@@ -673,7 +687,7 @@ bool Rasterizer::Rasterize(const ScanLineData2& scan_line_data2, int xsub, int y
     {
         ScanLineData::tSpanBuffer::const_iterator it = pOutline[i]->begin();
         ScanLineData::tSpanBuffer::const_iterator itEnd = pOutline[i]->end();
-        byte* plan_selected = i==0 ? overlay->mpOverlayBuffer.body : overlay->mpOverlayBuffer.border;
+        byte* plan_selected = i==0 ? body : border;
         int pitch = overlay->mOverlayPitch;
         for(; it!=itEnd; ++it)
         {
@@ -749,23 +763,40 @@ bool Rasterizer::Blur(const Overlay& input_overlay, int fBlur, double fGaussianB
 
     output_overlay->mOverlayPitch = (output_overlay->mOverlayWidth+15)&~15;
 
-    output_overlay->mpOverlayBuffer.base = (byte*)xy_malloc(2 * output_overlay->mOverlayPitch * output_overlay->mOverlayHeight);
-    memset(output_overlay->mpOverlayBuffer.base, 0, 2 * output_overlay->mOverlayPitch * output_overlay->mOverlayHeight);
-    output_overlay->mpOverlayBuffer.body = output_overlay->mpOverlayBuffer.base;
-    output_overlay->mpOverlayBuffer.border = output_overlay->mpOverlayBuffer.base + output_overlay->mOverlayPitch * output_overlay->mOverlayHeight;        
+    BYTE* body = reinterpret_cast<BYTE*>(xy_malloc(output_overlay->mOverlayPitch * output_overlay->mOverlayHeight));
+    if( body==NULL )
+    {
+        return false;
+    }
+    output_overlay->mBody.reset(body, xy_free);
+    memset(body, 0, output_overlay->mOverlayPitch * output_overlay->mOverlayHeight);
+    BYTE* border = NULL;
+    if (!output_overlay->mfWideOutlineEmpty)
+    {
+        border = reinterpret_cast<BYTE*>(xy_malloc(output_overlay->mOverlayPitch * output_overlay->mOverlayHeight));
+        if (border==NULL)
+        {
+            return false;
+        }
+        output_overlay->mBorder.reset(border, xy_free);
+        memset(border, 0, output_overlay->mOverlayPitch * output_overlay->mOverlayHeight);
+    }
 
     //copy buffer
     for(int i = 1; i >= 0; i--)
     {
-        byte* plan_selected = i==0 ? output_overlay->mpOverlayBuffer.body : output_overlay->mpOverlayBuffer.border;
-        const byte* plan_input = i==0 ? input_overlay.mpOverlayBuffer.body : input_overlay.mpOverlayBuffer.border;
+        byte* plan_selected = i==0 ? body : border;
+        const byte* plan_input = i==0 ? input_overlay.mBody.get() : input_overlay.mBorder.get();
 
         plan_selected += (bluradjust>>3) + (bluradjust>>3)*output_overlay->mOverlayPitch;
-        for (int j=0;j<input_overlay.mOverlayHeight;j++)
+        if ( plan_selected!=NULL && plan_input!=NULL )
         {
-            memcpy(plan_selected, plan_input, input_overlay.mOverlayPitch);
-            plan_selected += output_overlay->mOverlayPitch;
-            plan_input += input_overlay.mOverlayPitch;
+            for (int j=0;j<input_overlay.mOverlayHeight;j++)
+            {
+                memcpy(plan_selected, plan_input, input_overlay.mOverlayPitch);
+                plan_selected += output_overlay->mOverlayPitch;
+                plan_input += input_overlay.mOverlayPitch;
+            }
         }
     }
 
@@ -774,7 +805,7 @@ bool Rasterizer::Blur(const Overlay& input_overlay, int fBlur, double fGaussianB
     // Do some gaussian blur magic    
     if (fGaussianBlur > 0.1)//(fGaussianBlur > 0) return true even if fGaussianBlur very small
     {
-        byte* plan_selected= output_overlay->mfWideOutlineEmpty ? output_overlay->mpOverlayBuffer.body : output_overlay->mpOverlayBuffer.border;
+        byte* plan_selected= output_overlay->mfWideOutlineEmpty ? body : border;
         flyweight<key_value<double, ass_synth_priv, ass_synth_priv_key>, no_locking> fw_priv_blur(fGaussianBlur);
         const ass_synth_priv& priv_blur = fw_priv_blur.get();
         if (output_overlay->mOverlayWidth>=priv_blur.g_w && output_overlay->mOverlayHeight>=priv_blur.g_w)
@@ -789,7 +820,7 @@ bool Rasterizer::Blur(const Overlay& input_overlay, int fBlur, double fGaussianB
         if(output_overlay->mOverlayWidth >= 3 && output_overlay->mOverlayHeight >= 3)
         {            
             int pitch = output_overlay->mOverlayPitch;
-            byte* plan_selected= output_overlay->mfWideOutlineEmpty ? output_overlay->mpOverlayBuffer.body : output_overlay->mpOverlayBuffer.border;
+            byte* plan_selected= output_overlay->mfWideOutlineEmpty ? body : border;
             be_blur(plan_selected, tmp_buf.tmp, output_overlay->mOverlayWidth, output_overlay->mOverlayHeight, pitch);
         }
     }
@@ -1756,23 +1787,23 @@ void Overlay::FillAlphaMash( byte* outputAlphaMask, bool fBody, bool fBorder, in
 {
     if(!fBorder && fBody && pAlphaMask==NULL)
     {
-        _DoFillAlphaMash(outputAlphaMask, mpOverlayBuffer.body, NULL, x, y, w, h, pAlphaMask, pitch, color_alpha);        
+        _DoFillAlphaMash(outputAlphaMask, mBody.get(), NULL, x, y, w, h, pAlphaMask, pitch, color_alpha);        
     }
     else if(/*fBorder &&*/ fBody && pAlphaMask==NULL)
     {
-        _DoFillAlphaMash(outputAlphaMask, NULL, mpOverlayBuffer.border, x, y, w, h, pAlphaMask, pitch, color_alpha);        
+        _DoFillAlphaMash(outputAlphaMask, NULL, mBorder.get(), x, y, w, h, pAlphaMask, pitch, color_alpha);        
     }
     else if(!fBody && fBorder /* pAlphaMask==NULL or not*/)
     {
-        _DoFillAlphaMash(outputAlphaMask, mpOverlayBuffer.body, mpOverlayBuffer.border, x, y, w, h, pAlphaMask, pitch, color_alpha);        
+        _DoFillAlphaMash(outputAlphaMask, mBody.get(), mBorder.get(), x, y, w, h, pAlphaMask, pitch, color_alpha);        
     }
     else if(!fBorder && fBody && pAlphaMask!=NULL)
     {
-        _DoFillAlphaMash(outputAlphaMask, mpOverlayBuffer.body, NULL, x, y, w, h, pAlphaMask, pitch, color_alpha);        
+        _DoFillAlphaMash(outputAlphaMask, mBody.get(), NULL, x, y, w, h, pAlphaMask, pitch, color_alpha);        
     }
     else if(fBorder && fBody && pAlphaMask!=NULL)
     {
-        _DoFillAlphaMash(outputAlphaMask, NULL, mpOverlayBuffer.border, x, y, w, h, pAlphaMask, pitch, color_alpha);        
+        _DoFillAlphaMash(outputAlphaMask, NULL, mBorder.get(), x, y, w, h, pAlphaMask, pitch, color_alpha);        
     }
     else
     {
@@ -1800,27 +1831,47 @@ Overlay* Overlay::GetSubpixelVariance(unsigned int xshift, unsigned int yshift)
     overlay->mOverlayHeight = ((overlay->mHeight + 7)>>3) + 1;
     overlay->mOverlayPitch = (overlay->mOverlayWidth+15)&~15;
     
-    overlay->mpOverlayBuffer.base = reinterpret_cast<byte*>(xy_malloc(2 * overlay->mOverlayPitch * overlay->mOverlayHeight));
-    overlay->mpOverlayBuffer.body = overlay->mpOverlayBuffer.base;
-    overlay->mpOverlayBuffer.border = overlay->mpOverlayBuffer.base + overlay->mOverlayPitch * overlay->mOverlayHeight;
 
     overlay->mfWideOutlineEmpty = mfWideOutlineEmpty;
+
+    BYTE* body = reinterpret_cast<BYTE*>(xy_malloc(overlay->mOverlayPitch * overlay->mOverlayHeight));
+    if( body==NULL )
+    {
+        return false;
+    }
+    overlay->mBody.reset(body, xy_free);    
+    BYTE* border = NULL;
+    if (!overlay->mfWideOutlineEmpty)
+    {
+        border = reinterpret_cast<BYTE*>(xy_malloc(overlay->mOverlayPitch * overlay->mOverlayHeight));
+        if (border==NULL)
+        {
+            return false;
+        }
+        overlay->mBorder.reset(border, xy_free);        
+    }
     
-    if(overlay->mOverlayWidth==mOverlayWidth && overlay->mOverlayHeight==mOverlayHeight)
-        memcpy(overlay->mpOverlayBuffer.base, mpOverlayBuffer.base, 2 * mOverlayPitch * mOverlayHeight);
+    if(overlay->mOverlayPitch==mOverlayPitch && overlay->mOverlayHeight>=mOverlayHeight)
+    {
+        memcpy(body, mBody.get(), mOverlayPitch * mOverlayHeight);
+        memset(body+mOverlayPitch*mOverlayHeight, 0, mOverlayPitch * (overlay->mOverlayHeight-mOverlayHeight));
+        memcpy(border, mBorder.get(), mOverlayPitch * mOverlayHeight);
+        memset(border+mOverlayPitch*mOverlayHeight, 0, mOverlayPitch * (overlay->mOverlayHeight-mOverlayHeight));
+    }
     else
     {
-        memset(overlay->mpOverlayBuffer.base, 0, 2 * overlay->mOverlayPitch * overlay->mOverlayHeight);
-        byte* dst = overlay->mpOverlayBuffer.body;
-        const byte* src = mpOverlayBuffer.body;
+        memset(body, 0, overlay->mOverlayPitch * overlay->mOverlayHeight);
+        memset(border, 0, overlay->mOverlayPitch * overlay->mOverlayHeight);        
+        byte* dst = body;
+        const byte* src = mBody.get();
         for (int i=0;i<mOverlayHeight;i++)
         {
             memcpy(dst, src, mOverlayPitch);
             dst += overlay->mOverlayPitch;
             src += mOverlayPitch;
         }
-        dst = overlay->mpOverlayBuffer.border;
-        src = mpOverlayBuffer.border;
+        dst = border;
+        src = mBorder.get();
         for (int i=0;i<mOverlayHeight;i++)
         {
             memcpy(dst, src, mOverlayPitch);
@@ -1830,8 +1881,8 @@ Overlay* Overlay::GetSubpixelVariance(unsigned int xshift, unsigned int yshift)
     }
     //not equal
     //  Bilinear(overlay->mpOverlayBuffer.base, overlay->mOverlayWidth, 2*overlay->mOverlayHeight, overlay->mOverlayPitch, xshift, yshift);
-    Bilinear(overlay->mpOverlayBuffer.body, overlay->mOverlayWidth, overlay->mOverlayHeight, overlay->mOverlayPitch, xshift, yshift);
-    Bilinear(overlay->mpOverlayBuffer.border, overlay->mOverlayWidth, overlay->mOverlayHeight, overlay->mOverlayPitch, xshift, yshift);
+    Bilinear(body, overlay->mOverlayWidth, overlay->mOverlayHeight, overlay->mOverlayPitch, xshift, yshift);
+    Bilinear(border, overlay->mOverlayWidth, overlay->mOverlayHeight, overlay->mOverlayPitch, xshift, yshift);
     return overlay;
 }
 

src/subtitles/Rasterizer.h
@@ -151,7 +151,9 @@ struct Overlay
 public:
     Overlay()
     {
-        memset(this, 0, sizeof(*this));//todo: fix me. not safe.
+        mOffsetX=mOffsetY=mWidth=mHeight=0;
+        mOverlayWidth=mOverlayHeight=mOverlayPitch=0;
+        mfWideOutlineEmpty = false;
     }
     ~Overlay()
     {
@@ -159,9 +161,7 @@ public:
     }
 
     void CleanUp()
-    {
-        xy_free(mpOverlayBuffer.base);
-        memset(&mpOverlayBuffer, 0, sizeof(mpOverlayBuffer));        
+    {  
         mOverlayWidth=mOverlayHeight=mOverlayPitch=0;
         mfWideOutlineEmpty = false;
     }
@@ -171,12 +171,9 @@ public:
         const byte* pAlphaMask, int pitch, DWORD color_alpha);
 
     Overlay* GetSubpixelVariance(unsigned int xshift, unsigned int yshift);
-
-    struct {
-        byte *base;
-        byte *body;
-        byte *border;
-    } mpOverlayBuffer;
+public:
+    SharedPtrByte mBody;
+    SharedPtrByte mBorder;
     int mOffsetX, mOffsetY;
     int mWidth, mHeight;
         
@@ -191,15 +188,13 @@ private:
 
 typedef ::boost::shared_ptr<Overlay> SharedPtrOverlay;
 
-typedef ::boost::shared_array<BYTE> SharedArrayByte;
-
 struct GrayImage2
 {
 public:
     CPoint left_top;
     CSize size;
     int pitch;
-    SharedArrayByte data;
+    SharedPtrByte data;
 };
 
 typedef ::boost::shared_ptr<GrayImage2> SharedPtrGrayImage2;

src/subtitles/RTS.cpp
@@ -1170,37 +1170,13 @@ GrayImage2* CClipper::PaintSimpleClipper()
     CWord::Paint( m_polygon, CPoint(0, 0), CPoint(0, 0), &overlay_list );
     int w = overlay_list.overlay->mOverlayWidth, h = overlay_list.overlay->mOverlayHeight;
     int x = (overlay_list.overlay->mOffsetX+4)>>3, y = (overlay_list.overlay->mOffsetY+4)>>3;
-    int xo = 0, yo = 0;
-    if(x < 0) {xo = -x; w -= -x; x = 0;}
-    if(y < 0) {yo = -y; h -= -y; y = 0;}
-    if(x+w > m_size.cx) w = m_size.cx-x;
-    if(y+h > m_size.cy) h = m_size.cy-y;
-    if(w <= 0 || h <= 0) return result;
-
     result = new GrayImage2();
     if( !result )
         return result;
-    result->data.reset(new BYTE[w*h]);
-    result->pitch = w;
+    result->data = overlay_list.overlay->mBody;
+    result->pitch = overlay_list.overlay->mOverlayPitch;
     result->size.SetSize(w, h);
     result->left_top.SetPoint(x, y);
-
-    //fix me: unnecessary copy
-    BYTE * result_data = result->data.get();
-    if(!result_data)
-    {
-        delete result;
-        return NULL;
-    }
-    const BYTE* src = overlay_list.overlay->mpOverlayBuffer.body + (overlay_list.overlay->mOverlayPitch * yo + xo);
-    while(h--)
-    {
-        //for(int wt=0; wt<w; ++wt)
-        //  dst[wt] = src[wt];
-        memcpy(result_data, src, w);
-        src += overlay_list.overlay->mOverlayPitch;
-        result_data += w;
-    }
     return result;
 }
 
@@ -1225,7 +1201,7 @@ GrayImage2* CClipper::PaintBaseClipper()
     result = new GrayImage2();
     if( !result )
         return result;
-    result->data.reset(new BYTE[m_size.cx*m_size.cy]);
+    result->data.reset( reinterpret_cast<BYTE*>(xy_malloc(m_size.cx*m_size.cy)), xy_free );
     result->pitch = m_size.cx;
     result->size = m_size;
     result->left_top.SetPoint(0, 0);
@@ -1239,7 +1215,7 @@ GrayImage2* CClipper::PaintBaseClipper()
 
     memset( result_data, 0, m_size.cx*m_size.cy );
 
-    const BYTE* src = overlay_list.overlay->mpOverlayBuffer.body + (overlay_list.overlay->mOverlayPitch * yo + xo);
+    const BYTE* src = overlay_list.overlay->mBody.get() + (overlay_list.overlay->mOverlayPitch * yo + xo);
     BYTE* dst = result_data + m_size.cx * y + x;
     while(h--)
     {

commit efecf9a9bd5d8e706f66082ce3719b04069fe6b8

Fix corruption bug introduced in Commit:59d9decce762a384c815000ecd74911fea1b82e7.

src/subtitles/Rasterizer.cpp
@@ -1104,8 +1104,8 @@ SharedPtrByte Rasterizer::CompositeAlphaMask(SubPicDesc& spd, const SharedPtrOve
     //fix me: check and log error
     SharedPtrByte result;
     *outputDirtyRect = CRect(0, 0, 0, 0);
-    if(!switchpts || !fBody && !fBorder) return(result);
-
+    if (!switchpts || !fBody && !fBorder) return result;
+    if (fBorder && !overlay->mBorder) return result;
     // clip
     // Limit drawn area to intersection of rendering surface and rectangular clip area
     CRect r(0, 0, spd.w, spd.h);
@@ -1779,6 +1779,14 @@ void Overlay::_DoFillAlphaMash(byte* outputAlphaMask, const byte* pBody, const b
     {
         //should NOT happen!
         ASSERT(0);
+        while(h--)
+        {
+            for(int j=0;j<x_end;j++)
+            {
+                dst[j] = 0;
+            }
+            dst += mOverlayPitch;
+        }
     }
 }
 

src/subtitles/Rasterizer.h
@@ -161,7 +161,10 @@ public:
     }
 
     void CleanUp()
-    {  
+    {
+        mBody.reset((BYTE*)NULL);
+        mBorder.reset((BYTE*)NULL);
+        mOffsetX=mOffsetY=mWidth=mHeight=0;
         mOverlayWidth=mOverlayHeight=mOverlayPitch=0;
         mfWideOutlineEmpty = false;
     }

test/35.ass
@@ -1,0 +1,33 @@
+﻿[Script Info]
+; Script generated by Aegisub 2.1.8
+; http://www.aegisub.org/
+Title: YakuSub Studio, ep.11
+ScriptType: v4.00+
+WrapStyle: 0
+PlayResX: 1280
+PlayResY: 720
+ScaledBorderAndShadow: yes
+Last Style Storage: Default
+Video Aspect Ratio: 0
+Video Zoom: 6
+Video Position: 0
+Scroll Position: 0
+Active Line: 0
+YCbCr Matrix: None
+Collisions: Normal
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: OP_Rom,Georgia,39,&H00B96E1A,&H000000FF,&H00FAFAFA,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 2,Georgia,39,&H006852D4,&H000000FF,&H00FAFAFA,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 3,Georgia,39,&H002BD2E1,&H000000FF,&H00840DEF,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 4,Georgia,39,&H00242424,&H000000FF,&H00FFFFFF,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 5,Georgia,39,&H004FE0E1,&H000000FF,&H00001F12,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 6,Georgia,39,&H00000000,&H000000FF,&H00FAFAF8,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 7,Georgia,39,&H007F05ED,&H000000FF,&H00FAFAFA,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+Style: OP_Rom 8,Georgia,39,&H00111A5F,&H000000FF,&H00FAFAFA,&H96000000,-1,-1,0,0,100,100,0,0,1,3,1,8,17,17,10,1
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+Dialogue: 0,0:00:00.00,0:00:03.32,OP_Rom,,0000,0000,0000,,{\blur1\fscy0\t(0,115,\fscy120)}ko{\blur1\fscy0\t(230,355,\fscy120)}ko{\blur1\fscy0\t(480,600,\fscy120)}ro
+Dialogue: 0,0:00:03.32,0:00:05.32,OP_Rom,,0000,0000,0000,,