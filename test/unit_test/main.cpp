513a55e15c02dac63240eb4c56380bc3e05189e6
xy <yuzhuohuang@qq.com>
2012-07-28 13:11:59 +0800

    Blur Optimization. [Part 0] (unit test for blur code)

--- test/unit_test/main.cpp
+++ test/unit_test/main.cpp
@@ -2,10 +2,12 @@
 #include <gtest/gtest.h>
 #define XY_UNIT_TEST
 //#include "test_interlaced_uv_alphablend.h"
-#include "test_subsample_and_interlace.h"
-#include "test_alphablend.h"
-#include "test_instrinsics_macro.h"
+//#include "test_subsample_and_interlace.h"
+//#include "test_alphablend.h"
+//#include "test_instrinsics_macro.h"
 
+//#include "test_xy_filter.h"
+#include "xy_filter_benchmark.h"
 
 int main(int argc, char ** argv)
 {
