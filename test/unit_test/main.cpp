78048eeab71fede2a9af5f7073b40d60f1d18b36
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2012-07-31 19:16:08 +0800

    simple test tool

--- test/unit_test/main.cpp
+++ test/unit_test/main.cpp
@@ -1,5 +1,7 @@
 #include <afx.h>
 #include <gtest/gtest.h>
+#include <iostream>
+
 #define XY_UNIT_TEST
 //#include "test_interlaced_uv_alphablend.h"
 //#include "test_subsample_and_interlace.h"
@@ -7,10 +9,21 @@
 //#include "test_instrinsics_macro.h"
 
 //#include "test_xy_filter.h"
-#include "xy_filter_benchmark.h"
+//#include "xy_filter_benchmark.h"
+#include "test_overall.h"
 
-int main(int argc, char ** argv)
+
+int wmain(int argc, wchar_t ** argv)
 {
+    if (argc!=2)
+    {
+        std::wcout<<argv[0]<<L" script_name"<<std::endl;
+        return -1;
+    }
+    char namebuf[256];
+    WideCharToMultiByte(CP_UTF8, 0, argv[1], -1, namebuf, sizeof(namebuf)/sizeof(char), NULL, NULL);
+    OpenTestScript(namebuf);
+
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
