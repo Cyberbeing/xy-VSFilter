f577e30885f30b95ad99f03855b3977dae8b2ec9
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2012-07-31 19:16:08 +0800

    simple test tool

---
+++ test/unit_test/test_overall.h
@@ -0,0 +1,16 @@
+#ifndef __TEST_OVERALL_09562649_9C15_413C_81EA_6B6EE98E9CBB_H__
+#define __TEST_OVERALL_09562649_9C15_413C_81EA_6B6EE98E9CBB_H__
+
+#include <gtest/gtest.h>
+
+void OpenTestScript( const char *filename );
+void OverallTest(float fps = 25, int width=1280, int height=720,
+    double start=0, double end=60);
+
+TEST(OverallTest, xxx)
+{
+    OverallTest();
+}
+
+
+#endif // __TEST_OVERALL_09562649_9C15_413C_81EA_6B6EE98E9CBB_H__
