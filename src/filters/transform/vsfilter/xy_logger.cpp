f35d9f4c4f4c0b2eee726e1564d0a260ae58b3ed
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2012-10-25 19:59:39 +0800

    Add a log function

--- src/filters/transform/vsfilter/xy_logger.cpp
+++ src/filters/transform/vsfilter/xy_logger.cpp
@@ -3,6 +3,7 @@
 /* date: 20110512                                                       */
 /************************************************************************/
 #include "xy_logger.h"
+#include <fstream>
 
 #ifdef UNICODE
 
@@ -30,6 +31,29 @@ void doConfigure(const log4cplus::tstring& configFilename)
     unsigned flags = 0;
     log4cplus::PropertyConfigurator::doConfigure(configFilename, h, flags);
 #endif
+}
+
+void DumpPackBitmap2File(POINT pos, SIZE size, LPCVOID pixels, int pitch, const char *filename)
+{
+    using namespace std;
+
+    ofstream axxx(filename, ios_base::app);
+    int h = size.cy;
+    int w = size.cx;
+    const BYTE* src = reinterpret_cast<const BYTE*>(pixels);
+
+    axxx<<"pos:("<<pos.x<<","<<pos.y<<") size:("<<size.cx<<","<<size.cy<<")"<<std::endl;
+    for(int i=0;i<h;i++, src += pitch)
+    {
+        const BYTE* s2 = src;
+        const BYTE* s2end = s2 + w*4;
+        for(; s2 < s2end; s2 += 4)
+        {
+            axxx<<(int)s2[0]<<","<<(int)s2[1]<<","<<(int)s2[2]<<","<<(int)s2[3]<<",";
+        }
+        axxx<<std::endl;
+    }
+    axxx.close();
 }
 
 } //namespace xy_logger
