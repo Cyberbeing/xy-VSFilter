339a0aa0fbd4863ad091114fced92c138f3521e1
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2012-11-27 17:13:22 +0800

    Fix naming in about properties page

--- src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
+++ src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
@@ -1102,14 +1102,23 @@ CDVSAboutPPage::CDVSAboutPPage(LPUNKNOWN lpunk, HRESULT* phr)
 
 bool CDVSAboutPPage::OnMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    HRESULT hr = NOERROR;
     switch(uMsg) {
     case WM_INITDIALOG:
         {
             CStringA version_sha1_short = XY_VSFILTER_VERSION_COMMIT_SHA1;
             version_sha1_short = version_sha1_short.Left(7);
             CStringA version = XY_ABOUT_VERSION_STR;
-            version.Format("DirectVobSub %s (git %s)\nxy-VSFilter\nCopyright 2001-2012 Yu Zhuohuang, Gabest et. al.", 
-                XY_ABOUT_VERSION_STR, version_sha1_short);
+
+            LPWSTR name = NULL;
+            int chars = 0;
+            hr = m_pDirectVobSubXy->XyGetString(DirectVobSubXyOptions::STRING_NAME, &name, &chars);
+            CHECK_N_LOG(hr, "Failed to get option");
+            CStringW str_name(name, chars);
+            CoTaskMemFree(name);
+
+            version.Format("%ls %s (git %s)\nxy-VSFilter\nCopyright 2001-2012 Yu Zhuohuang, Gabest et. al.", 
+                str_name.GetString(), XY_ABOUT_VERSION_STR, version_sha1_short);
 
             SetDlgItemTextA( m_Dlg, IDC_VERSION, version.GetString() );
             break;
