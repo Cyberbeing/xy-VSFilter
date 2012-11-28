3725233906dbfb737a7885fbb1c98fea3427e471
yuzhuohuang@qq.com <yuzhuohuang@qq.com>
2012-11-28 15:55:51 +0800

    Cosmetics

--- src/subpic/PooledSubPic.cpp
+++ src/subpic/PooledSubPic.cpp
@@ -4,78 +4,78 @@
 
 STDMETHODIMP_(void) CPooledSubPicAllocator::ReleaseItem( void* Item )
 {
-	//DbgLog((LOG_TRACE, 3, "ReleaseItem::free:%d", _free.GetCount()));
-	CAutoLock lock(&_poolLock);
-	POSITION pos = _using.Find((CPooledSubPic*)Item);
-	if(pos!=NULL)
-	{
-		_using.RemoveAt(pos);
-		_free.AddTail((CPooledSubPic*)Item);
-	}
+    //DbgLog((LOG_TRACE, 3, "ReleaseItem::free:%d", _free.GetCount()));
+    CAutoLock lock(&_poolLock);
+    POSITION pos = _using.Find((CPooledSubPic*)Item);
+    if(pos!=NULL)
+    {
+        _using.RemoveAt(pos);
+        _free.AddTail((CPooledSubPic*)Item);
+    }
 }
 
 STDMETHODIMP_(void) CPooledSubPicAllocator::OnItemDestruct( void* Item )
 {
-	//DbgLog((LOG_TRACE, 3, "OnItemDestruct::free:%d", _free.GetCount()));
-	CAutoLock lock(&_poolLock);
-	//CPooledSubPic* typedItem = (CPooledSubPic*)Item;
-	POSITION pos = _using.Find((CPooledSubPic*)Item);
-	if(pos!=NULL)
-	{
-		_using.RemoveAt(pos);
-		_capacity--;
-	}
-	ASSERT(_free.Find((CPooledSubPic*)Item)==NULL);
+    //DbgLog((LOG_TRACE, 3, "OnItemDestruct::free:%d", _free.GetCount()));
+    CAutoLock lock(&_poolLock);
+    //CPooledSubPic* typedItem = (CPooledSubPic*)Item;
+    POSITION pos = _using.Find((CPooledSubPic*)Item);
+    if(pos!=NULL)
+    {
+        _using.RemoveAt(pos);
+        _capacity--;
+    }
+    ASSERT(_free.Find((CPooledSubPic*)Item)==NULL);
 }
 
 bool STDMETHODCALLTYPE CPooledSubPicAllocator::InitPool( int capacity )
 {
-	DbgLog((LOG_TRACE, 3, "%s(%d), %s", __FILE__, __LINE__, __FUNCTION__));
-	CAutoLock lock(&_poolLock);
-	CPooledSubPic* temp;
+    DbgLog((LOG_TRACE, 3, "%s(%d), %s", __FILE__, __LINE__, __FUNCTION__));
+    CAutoLock lock(&_poolLock);
+    CPooledSubPic* temp;
 
-	if(capacity<0)
-		capacity = 0;
+    if(capacity<0)
+        capacity = 0;
 
-	//while(capacity<_capacity)
+    //while(capacity<_capacity)
     while(_capacity>0)
-	{
-		if(!_free.IsEmpty())
-		{
-			temp = _free.RemoveTail();
-			_capacity--;
-			temp->_pool = NULL;
-			temp->Release();
-		}
-		else if(!_using.IsEmpty())
-		{
-			temp = _using.RemoveTail();
-			_capacity--;
-			temp->_pool = NULL;
-			temp->Release();
-		}
-	}
+    {
+        if(!_free.IsEmpty())
+        {
+            temp = _free.RemoveTail();
+            _capacity--;
+            temp->_pool = NULL;
+            temp->Release();
+        }
+        else if(!_using.IsEmpty())
+        {
+            temp = _using.RemoveTail();
+            _capacity--;
+            temp->_pool = NULL;
+            temp->Release();
+        }
+    }
 
-	while(_capacity<capacity)
-	{
-		if(!(temp = DoAlloc()))
-		{
-			ASSERT(0);
-			return(false);
-		}
+    while(_capacity<capacity)
+    {
+        if(!(temp = DoAlloc()))
+        {
+            ASSERT(0);
+            return(false);
+        }
 
-		_free.AddTail(temp);
-		_capacity++;
-		temp->AddRef();
-	}
-	//DbgLog((LOG_TRACE, 3, "InitPool::free:%d", _free.GetCount()));
-	return true;
+        _free.AddTail(temp);
+        _capacity++;
+        temp->AddRef();
+    }
+    //DbgLog((LOG_TRACE, 3, "InitPool::free:%d", _free.GetCount()));
+    return true;
 }
 
 CPooledSubPicAllocator::CPooledSubPicAllocator( int alpha_blt_dst_type, SIZE maxsize, int capacity, int type/*=-1*/ )
-	:CSubPicExAllocatorImpl(maxsize, false, false)
+    :CSubPicExAllocatorImpl(maxsize, false, false)
     , _alpha_blt_dst_type(alpha_blt_dst_type)
-	, _maxsize(maxsize)
+    , _maxsize(maxsize)
     , _type(type)
 {
     if(_type==-1)
@@ -101,23 +101,23 @@ CPooledSubPicAllocator::CPooledSubPicAllocator( int alpha_blt_dst_type, SIZE max
             break;
         }
     }
-	_capacity = 0;
-	InitPool(capacity);
+    _capacity = 0;
+    InitPool(capacity);
 }
 
 
 bool CPooledSubPicAllocator::AllocEx( bool fStatic, ISubPicEx** ppSubPic )
 {
-	if(!ppSubPic)
+    if(!ppSubPic)
     {
-		return(false);
+        return(false);
     }
-	{
-		CAutoLock lock(&_poolLock);
+    {
+        CAutoLock lock(&_poolLock);
         CollectUnUsedItem();
-		if(!_free.IsEmpty())
-		{
-			CPooledSubPic *item = _free.RemoveHead();
+        if(!_free.IsEmpty())
+        {
+            CPooledSubPic *item = _free.RemoveHead();
             if(item->m_spd.type!=_type)
             {
                 item->_pool = NULL;
@@ -125,43 +125,43 @@ bool CPooledSubPicAllocator::AllocEx( bool fStatic, ISubPicEx** ppSubPic )
                 item = DoAlloc();
                 item->AddRef();
             }
-			_using.AddTail(item);
-			*ppSubPic = item;
-			item->AddRef();
-		}
-	}
-	if(*ppSubPic!=NULL)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+            _using.AddTail(item);
+            *ppSubPic = item;
+            item->AddRef();
+        }
+    }
+    if(*ppSubPic!=NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 CPooledSubPicAllocator::~CPooledSubPicAllocator()
 {
-	CPooledSubPic* item = NULL;
-	CAutoLock lock(&_poolLock);
-	for(POSITION pos = _free.GetHeadPosition(); pos!=NULL; )
-	{
-		item = _free.GetNext(pos);
+    CPooledSubPic* item = NULL;
+    CAutoLock lock(&_poolLock);
+    for(POSITION pos = _free.GetHeadPosition(); pos!=NULL; )
+    {
+        item = _free.GetNext(pos);
         if(item!=NULL)
         {
-		    item->_pool = NULL;
-		    item->Release();
+            item->_pool = NULL;
+            item->Release();
         }
-	}
-	for(POSITION pos = _using.GetHeadPosition(); pos!=NULL; )
-	{
-		item = _using.GetNext(pos);
+    }
+    for(POSITION pos = _using.GetHeadPosition(); pos!=NULL; )
+    {
+        item = _using.GetNext(pos);
         if(item!=NULL)
         {
-		    item->_pool = NULL;
-		    item->Release();
+            item->_pool = NULL;
+            item->Release();
         }
-	}
+    }
 }
 
 STDMETHODIMP_(int) CPooledSubPicAllocator::SetSpdColorType( int color_type )
@@ -247,8 +247,8 @@ void CPooledSubPicAllocator::CollectUnUsedItem()
 
 CPooledSubPic::~CPooledSubPic()
 {
-	if(_pool!=NULL)
-		_pool->OnItemDestruct(this);
+    if(_pool!=NULL)
+        _pool->OnItemDestruct(this);
     xy_free(m_spd.bits);
     m_spd.bits = NULL;
 }
--- src/subpic/PooledSubPic.h
+++ src/subpic/PooledSubPic.h
@@ -9,32 +9,32 @@ using namespace std;
 class CPooledSubPic;
 interface IPooledAllocator
 {
-	STDMETHOD_(void, ReleaseItem)(void* Item) PURE;
-	STDMETHOD_(void, OnItemDestruct)(void* Item) PURE;
+    STDMETHOD_(void, ReleaseItem)(void* Item) PURE;
+    STDMETHOD_(void, OnItemDestruct)(void* Item) PURE;
 };
 
 class CPooledSubPicAllocator : public CSubPicExAllocatorImpl//, public IPooledAllocator
 {
 public:
-	STDMETHODIMP_(void) ReleaseItem(void* Item);
-	STDMETHODIMP_(void) OnItemDestruct(void* Item);
-	
-	bool STDMETHODCALLTYPE InitPool(int capacity);
+    STDMETHODIMP_(void) ReleaseItem(void* Item);
+    STDMETHODIMP_(void) OnItemDestruct(void* Item);
 
-	CPooledSubPicAllocator(int alpha_blt_dst_type, SIZE maxsize, int capacity, int type=-1 );
-	virtual ~CPooledSubPicAllocator();
+    bool STDMETHODCALLTYPE InitPool(int capacity);
+
+    CPooledSubPicAllocator(int alpha_blt_dst_type, SIZE maxsize, int capacity, int type=-1 );
+    virtual ~CPooledSubPicAllocator();
 private:
-	CCritSec _poolLock;
-	
-	int _type;
+    CCritSec _poolLock;
+
+    int _type;
     int _alpha_blt_dst_type;
-	CSize _maxsize;	
-	int _capacity;
-	CAtlList<CPooledSubPic*> _free;
-	CAtlList<CPooledSubPic*> _using;
+    CSize _maxsize;
+    int _capacity;
+    CAtlList<CPooledSubPic*> _free;
+    CAtlList<CPooledSubPic*> _using;
 
     CPooledSubPic* DoAlloc();
-	virtual bool AllocEx(bool fStatic, ISubPicEx** ppSubPic);
+    virtual bool AllocEx(bool fStatic, ISubPicEx** ppSubPic);
 
     void CollectUnUsedItem();
     STDMETHODIMP_(int) SetSpdColorType(int color_type);
@@ -43,12 +43,12 @@ private:
 
 class CPooledSubPic : public CMemSubPic
 {
-	friend class CPooledSubPicAllocator;
+    friend class CPooledSubPicAllocator;
 public:
-	CPooledSubPic(SubPicDesc& spd, int alpha_blt_dst_type, CPooledSubPicAllocator* pool):CMemSubPic(spd, alpha_blt_dst_type),_pool(pool){}
-	virtual ~CPooledSubPic();
+    CPooledSubPic(SubPicDesc& spd, int alpha_blt_dst_type, CPooledSubPicAllocator* pool):CMemSubPic(spd, alpha_blt_dst_type),_pool(pool){}
+    virtual ~CPooledSubPic();
     
 private:
-	CCritSec _csLock;
-	CPooledSubPicAllocator* _pool;
+    CCritSec _csLock;
+    CPooledSubPicAllocator* _pool;
 };
