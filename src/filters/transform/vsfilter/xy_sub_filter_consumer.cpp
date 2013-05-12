3154c975b091404bab839d68e067343cdb2af980
xy <yuzhuohuang@qq.com>
2013-05-12 17:25:06 +0800

    Consumer displays some provider info (layout/cache) in OSD

--- src/filters/transform/vsfilter/xy_sub_filter_consumer.cpp
+++ src/filters/transform/vsfilter/xy_sub_filter_consumer.cpp
@@ -1298,59 +1298,59 @@ void XySubFilterConsumer::PrintMessages(BYTE* pOut)
             m_xy_int_opt[INT_COLOR_SPACE]==CDirectVobSub::YuvMatrix_AUTO ? _T("Auto") :
             m_xy_int_opt[INT_COLOR_SPACE]==CDirectVobSub::GUESS ? _T("Guessed") : _T("Forced") );
         msg += tmp;
-
-        SIZE layout_size, script_playres;
-        XyGetSize(SIZE_LAYOUT_WITH, &layout_size);
-        XyGetSize(SIZE_ASS_PLAY_RESOLUTION, &script_playres);
-        tmp.Format( _T("Layout with size %dx%d, script playres %dx%d\n"),
-            layout_size.cx, layout_size.cy, script_playres.cx, script_playres.cy );
-        msg += tmp;
-
-        //print cache info
-        CachesInfo *caches_info = NULL;
-        XyFlyWeightInfo *xy_fw_info = NULL;
-        int tmp_size;
-        XyGetBin(DirectVobSubXyOptions::BIN2_CACHES_INFO, reinterpret_cast<LPVOID*>(&caches_info), &tmp_size);
-        XyGetBin(DirectVobSubXyOptions::BIN2_XY_FLY_WEIGHT_INFO, reinterpret_cast<LPVOID*>(&xy_fw_info), &tmp_size);
-        ASSERT(caches_info);
-        ASSERT(xy_fw_info);
-        tmp.Format(
-            _T("Cache :stored_num/hit_count/query_count\n")\
-            _T("  Parser cache 1:%ld/%ld/%ld\n")\
-            _T("  Parser cache 2:%ld/%ld/%ld\n")\
-            _T("\n")\
-            _T("  LV 4:%ld/%ld/%ld\t\t")\
-            _T("  LV 3:%ld/%ld/%ld\n")\
-            _T("  LV 2:%ld/%ld/%ld\t\t")\
-            _T("  LV 1:%ld/%ld/%ld\n")\
-            _T("  LV 0:%ld/%ld/%ld\n")\
-            _T("\n")\
-            _T("  bitmap   :%ld/%ld/%ld\t\t")\
-            _T("  scan line:%ld/%ld/%ld\n")\
-            _T("  relay key:%ld/%ld/%ld\t\t")\
-            _T("  clipper  :%ld/%ld/%ld\n")\
-            _T("\n")\
-            _T("  FW string pool    :%ld/%ld/%ld\t\t")\
-            _T("  FW bitmap key pool:%ld/%ld/%ld\n")\
-            ,
-            caches_info->text_info_cache_cur_item_num, caches_info->text_info_cache_hit_count, caches_info->text_info_cache_query_count,
-            caches_info->word_info_cache_cur_item_num, caches_info->word_info_cache_hit_count, caches_info->word_info_cache_query_count,
-            caches_info->path_cache_cur_item_num,     caches_info->path_cache_hit_count,     caches_info->path_cache_query_count,
-            caches_info->scanline_cache2_cur_item_num, caches_info->scanline_cache2_hit_count, caches_info->scanline_cache2_query_count,
-            caches_info->non_blur_cache_cur_item_num, caches_info->non_blur_cache_hit_count, caches_info->non_blur_cache_query_count,
-            caches_info->overlay_cache_cur_item_num,  caches_info->overlay_cache_hit_count,  caches_info->overlay_cache_query_count,
-            caches_info->interpolate_cache_cur_item_num, caches_info->interpolate_cache_hit_count, caches_info->interpolate_cache_query_count,
-            caches_info->bitmap_cache_cur_item_num, caches_info->bitmap_cache_hit_count, caches_info->bitmap_cache_query_count,
-            caches_info->scanline_cache_cur_item_num, caches_info->scanline_cache_hit_count, caches_info->scanline_cache_query_count,
-            caches_info->overlay_key_cache_cur_item_num, caches_info->overlay_key_cache_hit_count, caches_info->overlay_key_cache_query_count,
-            caches_info->clipper_cache_cur_item_num, caches_info->clipper_cache_hit_count, caches_info->clipper_cache_query_count,
-
-            xy_fw_info->xy_fw_string_w.cur_item_num, xy_fw_info->xy_fw_string_w.hit_count, xy_fw_info->xy_fw_string_w.query_count,
-            xy_fw_info->xy_fw_grouped_draw_items_hash_key.cur_item_num, xy_fw_info->xy_fw_grouped_draw_items_hash_key.hit_count, xy_fw_info->xy_fw_grouped_draw_items_hash_key.query_count
-            );
-        msg += tmp;
-        delete []caches_info;
 #endif
+        CComQIPtr<IXyOptions> provider = m_provider;
+        if (provider)
+        {
+            //print cache info
+            SIZE layout_size, script_playres;
+            provider->XyGetSize(SIZE_LAYOUT_WITH, &layout_size);
+            provider->XyGetSize(SIZE_ASS_PLAY_RESOLUTION, &script_playres);
+            tmp.Format( _T("Layout with size %dx%d, script playres %dx%d\n"),
+                layout_size.cx, layout_size.cy, script_playres.cx, script_playres.cy );
+            msg += tmp;
+
+            //print cache info
+            CachesInfo caches_info;
+            XyFlyWeightInfo xy_fw_info;
+            provider->XyGetBin2(DirectVobSubXyOptions::BIN2_CACHES_INFO, reinterpret_cast<LPVOID>(&caches_info), sizeof(caches_info));
+            provider->XyGetBin2(DirectVobSubXyOptions::BIN2_XY_FLY_WEIGHT_INFO, reinterpret_cast<LPVOID>(&xy_fw_info), sizeof(xy_fw_info));
+            tmp.Format(
+                _T("Cache :stored_num/hit_count/query_count\n")\
+                _T("  Parser cache 1:%ld/%ld/%ld\n")\
+                _T("  Parser cache 2:%ld/%ld/%ld\n")\
+                _T("\n")\
+                _T("  LV 4:%ld/%ld/%ld\t\t")\
+                _T("  LV 3:%ld/%ld/%ld\n")\
+                _T("  LV 2:%ld/%ld/%ld\t\t")\
+                _T("  LV 1:%ld/%ld/%ld\n")\
+                _T("  LV 0:%ld/%ld/%ld\n")\
+                _T("\n")\
+                _T("  bitmap   :%ld/%ld/%ld\t\t")\
+                _T("  scan line:%ld/%ld/%ld\n")\
+                _T("  relay key:%ld/%ld/%ld\t\t")\
+                _T("  clipper  :%ld/%ld/%ld\n")\
+                _T("\n")\
+                _T("  FW string pool    :%ld/%ld/%ld\t\t")\
+                _T("  FW bitmap key pool:%ld/%ld/%ld\n")\
+                ,
+                caches_info.text_info_cache_cur_item_num, caches_info.text_info_cache_hit_count, caches_info.text_info_cache_query_count,
+                caches_info.word_info_cache_cur_item_num, caches_info.word_info_cache_hit_count, caches_info.word_info_cache_query_count,
+                caches_info.path_cache_cur_item_num,     caches_info.path_cache_hit_count,     caches_info.path_cache_query_count,
+                caches_info.scanline_cache2_cur_item_num, caches_info.scanline_cache2_hit_count, caches_info.scanline_cache2_query_count,
+                caches_info.non_blur_cache_cur_item_num, caches_info.non_blur_cache_hit_count, caches_info.non_blur_cache_query_count,
+                caches_info.overlay_cache_cur_item_num,  caches_info.overlay_cache_hit_count,  caches_info.overlay_cache_query_count,
+                caches_info.interpolate_cache_cur_item_num, caches_info.interpolate_cache_hit_count, caches_info.interpolate_cache_query_count,
+                caches_info.bitmap_cache_cur_item_num, caches_info.bitmap_cache_hit_count, caches_info.bitmap_cache_query_count,
+                caches_info.scanline_cache_cur_item_num, caches_info.scanline_cache_hit_count, caches_info.scanline_cache_query_count,
+                caches_info.overlay_key_cache_cur_item_num, caches_info.overlay_key_cache_hit_count, caches_info.overlay_key_cache_query_count,
+                caches_info.clipper_cache_cur_item_num, caches_info.clipper_cache_hit_count, caches_info.clipper_cache_query_count,
+
+                xy_fw_info.xy_fw_string_w.cur_item_num, xy_fw_info.xy_fw_string_w.hit_count, xy_fw_info.xy_fw_string_w.query_count,
+                xy_fw_info.xy_fw_grouped_draw_items_hash_key.cur_item_num, xy_fw_info.xy_fw_grouped_draw_items_hash_key.hit_count, xy_fw_info.xy_fw_grouped_draw_items_hash_key.query_count
+                );
+            msg += tmp;
+        }
     }
 
     if(msg.IsEmpty()) return;
