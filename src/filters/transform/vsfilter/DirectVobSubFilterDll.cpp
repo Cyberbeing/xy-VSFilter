adcfc4ea0ead28640c379841d5bf2adb6cbf7f87
xy <yuzhuohuang@qq.com>
2013-05-14 22:49:36 +0800

    Remove unused VMRZOOMENABLED reg option. Why it is there?

--- src/filters/transform/vsfilter/DirectVobSubFilterDll.cpp
+++ src/filters/transform/vsfilter/DirectVobSubFilterDll.cpp
@@ -107,9 +107,6 @@ STDAPI DllRegisterServer()
     if(theApp.GetProfileInt(ResStr(IDS_R_GENERAL), ResStr(IDS_RG_SEENDIVXWARNING), 0) != 1)
         theApp.WriteProfileInt(ResStr(IDS_R_GENERAL), ResStr(IDS_RG_SEENDIVXWARNING), 0);
 
-    if(theApp.GetProfileInt(ResStr(IDS_R_GENERAL), ResStr(IDS_RG_VMRZOOMENABLED), -1) == -1)
-        theApp.WriteProfileInt(ResStr(IDS_R_GENERAL), ResStr(IDS_RG_VMRZOOMENABLED), 0);
-
     /*removeme*/
     JajDeGonoszVagyok();
 
