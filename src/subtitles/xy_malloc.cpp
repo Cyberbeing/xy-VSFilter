7a5973570bfa09c462c181ccddbc41d3e447c8d1
xy <yuzhuohuang@qq.com>
2013-08-23 08:34:00 +0800

    Minor refinement: 1) use size::size_t instead of int so that insane block size is possible in 64-bit build 2) do not crash in side xy_malloc

--- src/subtitles/xy_malloc.cpp
+++ src/subtitles/xy_malloc.cpp
@@ -11,7 +11,7 @@
 /****************************************************************************
  * xy_malloc:
  ****************************************************************************/
-void *xy_malloc( int i_size, int align_shift )
+void *xy_malloc( std::size_t i_size, int align_shift )
 {
     const int ALIGNMENT = 16;
     const int MASK = ALIGNMENT - 1;
@@ -19,11 +19,15 @@ void *xy_malloc( int i_size, int align_shift )
     uint8_t * align_buf;
     align_shift &= MASK;
     buf = (uint8_t *) malloc( i_size + MASK + sizeof( void ** ) +
-              sizeof( int ) );
-    align_buf = buf + MASK + sizeof( void ** ) + sizeof( int );
+              sizeof( i_size ) );
+    if (!buf)
+    {
+        return NULL;
+    }
+    align_buf = buf + MASK + sizeof( void ** ) + sizeof( i_size );
     align_buf -= (ALIGNMENT + ((intptr_t) align_buf & MASK) - align_shift) & MASK;
     *( (void **) ( align_buf - sizeof( void ** ) ) ) = buf;
-    *( (int *) ( align_buf - sizeof( void ** ) - sizeof( int ) ) ) = i_size;
+    *( (std::size_t *) ( align_buf - sizeof( void ** ) - sizeof( i_size ) ) ) = i_size;
     return align_buf;
 }
 
@@ -41,21 +45,18 @@ void xy_free( void *p )
 /****************************************************************************
  * xy_realloc:
  ****************************************************************************/
-void *xy_realloc( void *p, int i_size, int align_shift )
+void *xy_realloc( void *p, std::size_t i_size, int align_shift )
 {
-    int       i_old_size = 0;
-    uint8_t * p_new;
-    if( p )
-    {
-        i_old_size = *( (int*) ( (uint8_t*) p - sizeof( void ** ) -
-                         sizeof( int ) ) );
-    }
+    std::size_t i_old_size = 0;
+    uint8_t    *p_new = NULL;
     p_new = (uint8_t*)xy_malloc( i_size, align_shift );
-    if( i_old_size > 0 && i_size > 0 )
+    if (p_new && p)
     {
+        i_old_size = *( (std::size_t*) ( (uint8_t*) p - sizeof( void ** ) -
+            sizeof( i_size ) ) );
         memcpy( p_new, p, ( i_old_size < i_size ) ? i_old_size : i_size );
+        xy_free(p);
     }
-    xy_free( p );
     return p_new;
 }
 
--- src/subtitles/xy_malloc.h
+++ src/subtitles/xy_malloc.h
@@ -5,10 +5,12 @@
 #ifndef __XY_MALLOC_H_22FA5F7D_A5C3_4D8D_B4E6_5FB954770019__
 #define __XY_MALLOC_H_22FA5F7D_A5C3_4D8D_B4E6_5FB954770019__
 
+#include <cstddef>
+
 /* xy_malloc : will do or emulate a memalign
  * you have to use xy_free for buffers allocated with xy_malloc */
-void *xy_malloc( int size, int align_shift=0);
-void *xy_realloc( void *p, int i_size, int align_shift=0 );
+void *xy_malloc( std::size_t size, int align_shift=0);
+void *xy_realloc( void *p, std::size_t i_size, int align_shift=0 );
 void  xy_free( void * );
 
 #endif // end of __XY_MALLOC_H_22FA5F7D_A5C3_4D8D_B4E6_5FB954770019__
