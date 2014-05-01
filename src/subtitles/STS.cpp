383f9674a4a3e5c37debb947fccda88f7560621b
Thomas Goyne <tgoyne@gmail.com>
2014-05-01 07:39:20 -0700

    Mark stuff not used outside of STS.cpp as static

--- src/subtitles/STS.cpp
+++ src/subtitles/STS.cpp
@@ -45,7 +45,7 @@
 
 // gathered from http://www.netwave.or.jp/~shikai/shikai/shcolor.htm
 
-struct htmlcolor {TCHAR* name; DWORD color;} hmtlcolors[] =
+static struct htmlcolor {TCHAR* name; DWORD color;} hmtlcolors[] =
 {
     {_T("white"), 0xffffff},
     {_T("whitesmoke"), 0xf5f5f5},
@@ -292,7 +292,7 @@ static DWORD CharSetToCodePage(DWORD dwCharSet)
     return cs.ciACP;
 }
 
-int FindChar(CStringW str, WCHAR c, int pos, bool fUnicode, int CharSet)
+static int FindChar(CStringW str, WCHAR c, int pos, bool fUnicode, int CharSet)
 {
     if(fUnicode) return(str.Find(c, pos));
 
@@ -495,7 +495,7 @@ static CStringW MBCSSSAToUnicode(CStringW str, int CharSet)
     return(ret);
 }
 
-CStringW RemoveSSATags(CStringW str, bool fUnicode, int CharSet)
+static CStringW RemoveSSATags(CStringW str, bool fUnicode, int CharSet)
 {
     str.Replace (L"{\\i1}", L"<i>");
     str.Replace (L"{\\i}", L"</i>");
@@ -1222,7 +1222,7 @@ static bool OpenVPlayer(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet)
     return(!ret.IsEmpty());
 }
 
-inline CStringW GetStr(CStringW& buff, char sep = ',') //throw(...)
+static inline CStringW GetStr(CStringW& buff, char sep = ',') //throw(...)
 {
     buff.TrimLeft();
 
@@ -1239,7 +1239,7 @@ inline CStringW GetStr(CStringW& buff, char sep = ',') //throw(...)
     return(ret);
 }
 
-inline int GetInt(CStringW& buff, char sep = ',') //throw(...)
+static inline int GetInt(CStringW& buff, char sep = ',') //throw(...)
 {
     CStringW str;
 
@@ -1256,7 +1256,7 @@ inline int GetInt(CStringW& buff, char sep = ',') //throw(...)
     return(ret);
 }
 
-inline double GetFloat(CStringW& buff, char sep = ',') //throw(...)
+static inline double GetFloat(CStringW& buff, char sep = ',') //throw(...)
 {
     CStringW str;
 
@@ -1269,7 +1269,7 @@ inline double GetFloat(CStringW& buff, char sep = ',') //throw(...)
     return((double)ret);
 }
 
-inline CStringW::PCXSTR TryNextStr(CStringW::PXSTR * buff, WCHAR sep = WCHAR(','))
+static inline CStringW::PCXSTR TryNextStr(CStringW::PXSTR * buff, WCHAR sep = WCHAR(','))
 {
     CStringW::PXSTR start = NULL;
     CStringW::PXSTR ret = NULL;
@@ -1288,7 +1288,7 @@ inline CStringW::PCXSTR TryNextStr(CStringW::PXSTR * buff, WCHAR sep = WCHAR(','
     return(ret);
 }
 
-inline int NextInt(CStringW::PXSTR * buff, WCHAR sep = WCHAR(',')) //throw(...)
+static inline int NextInt(CStringW::PXSTR * buff, WCHAR sep = WCHAR(',')) //throw(...)
 {
     CStringW::PCXSTR str = TryNextStr(buff, sep);
 
@@ -1304,7 +1304,7 @@ inline int NextInt(CStringW::PXSTR * buff, WCHAR sep = WCHAR(',')) //throw(...)
     return(ret);
 }
 
-inline double NextFloat(CStringW::PXSTR * buff, WCHAR sep = WCHAR(',')) //throw(...)
+static inline double NextFloat(CStringW::PXSTR * buff, WCHAR sep = WCHAR(',')) //throw(...)
 {
     CStringW str;
 
