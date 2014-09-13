cf474e6f84da6ebac580aefb4e8368e2934a3db5
Bruno Barbieri <brunorex@gmail.com>
2014-09-13 20:55:22 -0300

    Update UnRAR to v5.1.7

--- src/thirdparty/unrar/ulinks.cpp
+++ src/thirdparty/unrar/ulinks.cpp
@@ -1,6 +1,6 @@
 
 
-static bool UnixSymlink(const char *Target,const wchar *LinkName)
+static bool UnixSymlink(const char *Target,const wchar *LinkName,RarTime *ftm,RarTime *fta)
 {
   CreatePath(LinkName,true);
   DelFile(LinkName);
@@ -17,9 +17,15 @@ static bool UnixSymlink(const char *Target,const wchar *LinkName)
     }
     return false;
   }
-  // We do not set time of created symlink, because utime changes
-  // time of link target and lutimes is not available on all Linux
-  // systems at the moment of writing this code.
+#ifdef USE_LUTIMES
+  struct timeval tv[2];
+  tv[0].tv_sec=fta->GetUnix();
+  tv[0].tv_usec=long(fta->GetRaw()%10000000/10);
+  tv[1].tv_sec=ftm->GetUnix();
+  tv[1].tv_usec=long(ftm->GetRaw()%10000000/10);
+  lutimes(LinkNameA,tv);
+#endif
+
   return true;
 }
 
@@ -42,7 +48,7 @@ bool ExtractUnixLink30(ComprDataIO &DataIO,Archive &Arc,const wchar *LinkName)
     if (!DataIO.UnpHash.Cmp(&Arc.FileHead.FileHash,Arc.FileHead.UseHashKey ? Arc.FileHead.HashKey:NULL))
       return true;
 
-    return UnixSymlink(Target,LinkName);
+    return UnixSymlink(Target,LinkName,&Arc.FileHead.mtime,&Arc.FileHead.atime);
   }
   return false;
 }
@@ -55,10 +61,12 @@ bool ExtractUnixLink50(const wchar *Name,FileHeader *hd)
   if (hd->RedirType==FSREDIR_WINSYMLINK || hd->RedirType==FSREDIR_JUNCTION)
   {
     // Cannot create Windows absolute path symlinks in Unix. Only relative path
-    // Windows symlinks can be created here.
-    if (strncmp(Target,"\\??\\",4)==0)
+    // Windows symlinks can be created here. RAR 5.0 used \??\ prefix
+    // for Windows absolute symlinks, since RAR 5.1 /??/ is used.
+    // We escape ? as \? to avoid "trigraph" warning
+    if (strncmp(Target,"\\??\\",4)==0 || strncmp(Target,"/\?\?/",4)==0)
       return false;
     DosSlashToUnix(Target,Target,ASIZE(Target));
   }
-  return UnixSymlink(Target,Name);
+  return UnixSymlink(Target,Name,&hd->mtime,&hd->atime);
 }
--- src/thirdparty/unrar/version.hpp
+++ src/thirdparty/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
-#define RARVER_MINOR    10
+#define RARVER_MINOR    11
 #define RARVER_BETA      1
-#define RARVER_DAY      17
-#define RARVER_MONTH     3
+#define RARVER_DAY       6
+#define RARVER_MONTH     8
 #define RARVER_YEAR   2014
