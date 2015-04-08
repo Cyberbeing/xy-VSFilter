0b333354c5884a8ae14b6c3805121b7ae937c1e1
Underground78 <underground78@users.sourceforge.net>
2015-04-07 17:27:27 -0700

    STS::Add: Some segments were forgotten when inserting.

--- src/subtitles/STS.cpp
+++ src/subtitles/STS.cpp
@@ -2068,7 +2068,8 @@ void CSimpleTextSubtitle::Add(CStringW str, bool fUnicode, int start, int end,
             m_segments.InsertAt(0, stss);
         }
 
-        for(size_t i = 0; i < m_segments.GetCount(); i++)
+        size_t i;
+        for(i = 0; i < m_segments.GetCount(); i++)
         {
             STSSegment& s = m_segments[i];
 
@@ -2126,11 +2127,11 @@ void CSimpleTextSubtitle::Add(CStringW str, bool fUnicode, int start, int end,
             }
         }
 
-        if(end > m_segments[m_segments.GetCount()-1].end)
+        if(end > m_segments[i - 1].end)
         {
-            STSSegment stss(m_segments[m_segments.GetCount()-1].end, end);
+            STSSegment stss(m_segments[i - 1].end, end);
             stss.subs.Add(n);
-            m_segments.Add(stss);
+            m_segments.InsertAt(i, stss);
         }
     }
 }
