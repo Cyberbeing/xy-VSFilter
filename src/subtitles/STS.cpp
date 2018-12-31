Cyberbeing/xy-VSFilter

commit 02440d472def3a9e0b2ce84f958c8b0c0aa3ad7f

Rewrite CTextFile::ReadString
Drop ANSI support since it was used nowhere, purge stuff for non-Unicode
builds since it's been a very long time since there was any reason to do
such a thing, and make it not absurdly slow.

Reduces the time to load an arbitrary test file from 824ms to 180ms.

src/subtitles/STS.cpp
@@ -328,14 +328,7 @@ static int FindChar(CStringW str, WCHAR c, int pos, bool fUnicode, int CharSet)
 
     return(-1);
 }
-/*
-int FindChar(CStringA str, char c, int pos, bool fUnicode, int CharSet)
-{
-    ASSERT(!fUnicode);
 
-    return(FindChar(AToW(str), c, pos, false, CharSet));
-}
-*/
 static CStringW ToMBCS(CStringW str, DWORD CharSet)
 {
     CStringW ret;
@@ -661,7 +654,7 @@ static bool OpenSubViewer(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet
                 i = j;
 
                 if(tag == L"font")
-                    font = def.fontName.CompareNoCase(WToT(param)) ? param : L"";
+                    font = def.fontName.CompareNoCase(param) ? param : L"";
                 else if(tag == L"colf")
                     color = def.colors[0] != wcstol(((LPCWSTR)param)+2, 0, 16) ? param : L"";
                 else if(tag == L"size")
@@ -925,7 +918,7 @@ static bool OpenMicroDVD(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet)
             int i;
             if(buff.Find('{') == 0 && (i = buff.Find('}')) > 1 && i < buff.GetLength())
             {
-                if(STSStyle* s = GetMicroDVDStyle(WToT(buff.Mid(i+1)), CharSet))
+                if(STSStyle* s = GetMicroDVDStyle(buff.Mid(i+1), CharSet))
                 {
                     style = buff.Mid(1, i-1);
                     style.MakeUpper();
@@ -1083,7 +1076,7 @@ static CStringW SMI2SSA(CStringW str, int CharSet)
             {
                 DWORD color;
 
-                CString key = WToT(arg);
+                CString key = arg;
                 void* val;
                 if(g_colors.Lookup(key, val)) color = (DWORD)val;
                 else color = wcstol(arg, NULL, 16);
@@ -1461,17 +1454,17 @@ static bool OpenSubStationAlpha(CTextFile* file, CSimpleTextSubtitle& ret, int C
                 mm2 = NextInt(&__buff, ':');
                 ss2 = NextInt(&__buff, '.');
                 ms2_div10 = NextInt(&__buff);
-                Style = WToT(TryNextStr(&__buff));
-                Actor = WToT(TryNextStr(&__buff));
+                Style = TryNextStr(&__buff);
+                Actor = TryNextStr(&__buff);
                 marginRect.left = NextInt(&__buff);
                 marginRect.right = NextInt(&__buff);
                 marginRect.top = marginRect.bottom = NextInt(&__buff);
                 if(version >= 6)marginRect.bottom = NextInt(&__buff);
-                Effect = WToT(TryNextStr(&__buff));
+                Effect = TryNextStr(&__buff);
 
                 CStringW buff2 = __buff;
                 int len = min(Effect.GetLength(), buff2.GetLength());
-                if(Effect.Left(len) == WToT(buff2.Left(len))) Effect.Empty();
+                if(Effect.Left(len) == buff2.Left(len)) Effect.Empty();
 
                 Style.TrimLeft('*');
                 if(!Style.CompareNoCase(_T("Default"))) Style = _T("Default");
@@ -1568,8 +1561,8 @@ static bool OpenSubStationAlpha(CTextFile* file, CSimpleTextSubtitle& ret, int C
                 int alpha;
                 CRect tmp_rect;
 
-                StyleName = WToT(GetStr(buff));
-                style->fontName = WToT(GetStr(buff));
+                StyleName = GetStr(buff);
+                style->fontName = GetStr(buff);
                 style->fontSize = GetFloat(buff);
                 for(int i = 0; i < 4; i++) style->colors[i] = (COLORREF)GetInt(buff);
                 style->fontWeight = !!GetInt(buff) ? FW_BOLD : FW_NORMAL;
@@ -1722,8 +1715,8 @@ static bool OpenXombieSub(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet
                 CString StyleName;
                 CRect tmp_rect;
 
-                StyleName = WToT(GetStr(buff)) + _T("_") + WToT(GetStr(buff));
-                style->fontName = WToT(GetStr(buff));
+                StyleName = GetStr(buff) + _T("_") + GetStr(buff);
+                style->fontName = GetStr(buff);
                 style->fontSize = GetFloat(buff);
                 for(int i = 0; i < 4; i++) style->colors[i] = (COLORREF)GetInt(buff);
                 for(int i = 0; i < 4; i++) style->alpha[i] = GetInt(buff);
@@ -1787,8 +1780,8 @@ static bool OpenXombieSub(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet
                 mm2 = GetInt(buff, ':');
                 ss2 = GetInt(buff, '.');
                 ms2 = GetInt(buff);
-                Style = WToT(GetStr(buff)) + _T("_") + WToT(GetStr(buff));
-                Actor = WToT(GetStr(buff));
+                Style = GetStr(buff) + _T("_") + GetStr(buff);
+                Actor = GetStr(buff);
                 marginRect.left = GetInt(buff);
                 marginRect.right = GetInt(buff);
                 marginRect.top = marginRect.bottom = GetInt(buff);
@@ -2567,11 +2560,6 @@ void CSimpleTextSubtitle::ConvertUnicode(int i, bool fUnicode)
     }
 }
 
-CStringA CSimpleTextSubtitle::GetStrA(int i, bool fSSA)
-{
-    return(WToA(GetStrWA(i, fSSA)));
-}
-
 CStringW CSimpleTextSubtitle::GetStrW(int i, bool fSSA)
 {
     bool fUnicode = IsEntryUnicode(i);
@@ -2602,11 +2590,6 @@ CStringW CSimpleTextSubtitle::GetStrWA(int i, bool fSSA)
         str = RemoveSSATags(str, fUnicode, CharSet);
 
     return(str);
-}
-
-void CSimpleTextSubtitle::SetStr(int i, CStringA str, bool fUnicode)
-{
-    SetStr(i, AToW(str), false);
 }
 
 void CSimpleTextSubtitle::SetStr(int i, CStringW str, bool fUnicode)
@@ -3002,9 +2985,9 @@ bool CSimpleTextSubtitle::SaveAs(CString fn, exttype et, double fps, CTextFile::
             str2.Format(fmt,
                 hh1, mm1, ss1, ms1/10,
                 hh2, mm2, ss2, ms2/10,
-                TToW(stse.style), TToW(stse.actor),
+                stse.style, stse.actor,
                 stse.marginRect.left, stse.marginRect.right, (stse.marginRect.top + stse.marginRect.bottom) / 2,
-                TToW(stse.effect), str);
+                stse.effect, str);
         }
         else if(et == EXTASS)
         {
@@ -3013,9 +2996,9 @@ bool CSimpleTextSubtitle::SaveAs(CString fn, exttype et, double fps, CTextFile::
                 stse.layer,
                 hh1, mm1, ss1, ms1/10,
                 hh2, mm2, ss2, ms2/10,
-                TToW(stse.style), TToW(stse.actor),
+                stse.style, stse.actor,
                 stse.marginRect.left, stse.marginRect.right, (stse.marginRect.top + stse.marginRect.bottom) / 2,
-                TToW(stse.effect), str);
+                stse.effect, str);
         }
 
         f.WriteString(str2);
@@ -3244,7 +3227,7 @@ STSStyle& operator <<= (STSStyle& s, const CString& style)
 
     try
     {
-        CStringW str = TToW(style);
+        CStringW str = style;
         if(str.Find(';')>=0)
         {
             CRect tmp_rect;
@@ -3255,11 +3238,11 @@ STSStyle& operator <<= (STSStyle& s, const CString& style)
             for(int i = 0; i < 4; i++) s.colors[i] = (COLORREF)GetInt(str,';');
             for(int i = 0; i < 4; i++) s.alpha[i] = GetInt(str,';');
             s.charSet     = GetInt(str,';');
-            s.fontName    = WToT(GetStr(str,';')); s.fontSize   = GetFloat(str,';');
-            s.fontScaleX  = GetFloat(str,';');     s.fontScaleY = GetFloat(str,';');
-            s.fontSpacing = GetFloat(str,';');     s.fontWeight = GetInt  (str,';');
-            s.fItalic     = !!GetInt(str,';');     s.fUnderline = !!GetInt(str,';'); s.fStrikeOut = !!GetInt(str,';'); s.fBlur = GetFloat(str,';'); s.fGaussianBlur = GetFloat(str,';');
-            s.fontAngleZ  = GetFloat(str,';');     s.fontAngleX = GetFloat(str,';'); s.fontAngleY = GetFloat(str,';');
+            s.fontName    = GetStr(str,';');   s.fontSize   = GetFloat(str,';');
+            s.fontScaleX  = GetFloat(str,';'); s.fontScaleY = GetFloat(str,';');
+            s.fontSpacing = GetFloat(str,';'); s.fontWeight = GetInt  (str,';');
+            s.fItalic     = !!GetInt(str,';'); s.fUnderline = !!GetInt(str,';'); s.fStrikeOut = !!GetInt(str,';'); s.fBlur = GetFloat(str,';'); s.fGaussianBlur = GetFloat(str,';');
+            s.fontAngleZ  = GetFloat(str,';'); s.fontAngleX = GetFloat(str,';'); s.fontAngleY = GetFloat(str,';');
             s.relativeTo  = GetInt(str,';');
         }
     }

src/subtitles/RTS.cpp
@@ -2868,7 +2868,7 @@ bool CRenderedTextSubtitle::ParseSSATag( CSubtitle* sub, const AssTagList& assTa
         case CMD_r:
             {
                 STSStyle* val;
-                style = (!p.IsEmpty() && m_styles.Lookup(WToT(p), val) && val) ? *val : org;
+                style = (!p.IsEmpty() && m_styles.Lookup(p, val) && val) ? *val : org;
                 break;
             }
         case CMD_shad:
@@ -3073,7 +3073,7 @@ bool CRenderedTextSubtitle::ParseHtmlTag(CSubtitle* sub, CStringW str, STSStyle&
                 }
                 if(nColor >= 0 && nColor < 4)
                 {
-                    CString key = WToT(params[i]).TrimLeft(L'#');
+                    CString key = params[i].TrimLeft(L'#');
                     DWORD val;
                     if(g_colors.Lookup(key, val))
                         style.colors[nColor] = val;

src/subtitles/STS.h
@@ -276,17 +276,11 @@ public:
     bool      IsEntryUnicode(int i);
     void      ConvertUnicode(int i, bool fUnicode);
 
-    CStringA GetStrA (int i, bool fSSA = false);
     CStringW GetStrW (int i, bool fSSA = false);
     CStringW GetStrWA(int i, bool fSSA = false);
 
-#ifdef UNICODE
 #  define GetStr GetStrW
-#else
-#  define GetStr GetStrA
-#endif
 
-    void SetStr(int i, CStringA str, bool fUnicode /* ignored */);
     void SetStr(int i, CStringW str, bool fUnicode);
 
     friend bool OpenMicroDVD(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet);

src/subtitles/SubtitleInputPin.cpp
@@ -180,7 +180,7 @@ STDMETHODIMP CTextSubtitleInputPinHepler::Receive( IMediaSample* pSample )
                 }
                 else if(tag == __GAB1_ENTRY__)
                 {
-                    m_pRTS->Add(AToW(&ptr[8]), false, *(int*)ptr, *(int*)(ptr+4));
+                    m_pRTS->Add((LPWSTR)CA2WEX<>(ptr), false, *(int*)ptr, *(int*)(ptr+4));
                 }
                 else if(tag == __GAB1_LANGUAGE_UNICODE__)
                 {
@@ -225,7 +225,7 @@ STDMETHODIMP CTextSubtitleInputPinHepler::Receive( IMediaSample* pSample )
 
             if(!str.IsEmpty())
             {
-                m_pRTS->Add(AToW(str), false, (int)(tStart / 10000), (int)(tStop / 10000));
+                m_pRTS->Add((LPWSTR)CA2WEX<>(str), false, (int)(tStart / 10000), (int)(tStop / 10000));
             }
         }
         else

commit 5bacc69416485ac98ed035d697a97f662735e422

Fix: don't ignore the very last line when no LF after that for SRT & SSA/ASS Regression from 02440d47

src/subtitles/STS.cpp
@@ -524,7 +524,7 @@ static CStringW SubRipper2SSA(CStringW str, int CharSet)
 static bool OpenSubRipper(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet)
 {
     CStringW buff;
-    while (file->ReadString(buff)) {
+    while(file->ReadString(buff)) {
         FastTrim(buff);
         if (buff.IsEmpty()) {
             continue;
@@ -553,7 +553,10 @@ static bool OpenSubRipper(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet
 
             bool fFoundEmpty = false;
 
-            while (file->ReadString(tmp)) {
+            while(true) {
+                bool fEOF = !file->ReadString(tmp);
+                if (fEOF && tmp.IsEmpty()) break;
+
                 FastTrim(tmp);
                 if (tmp.IsEmpty()) {
                     fFoundEmpty = true;
@@ -567,6 +570,8 @@ static bool OpenSubRipper(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet
                 }
 
                 str += tmp + '\n';
+
+                if(fEOF) break;
             }
 
             ret.Add(
@@ -1424,8 +1429,11 @@ static bool OpenSubStationAlpha(CTextFile* file, CSimpleTextSubtitle& ret, int C
     ret.m_eYCbCrRange = CSimpleTextSubtitle::YCbCrRange_TV;
 
     CStringW buff;
-    while(file->ReadString(buff))
+    while(true)
     {
+    	bool fEOF = !file->ReadString(buff);
+    	if(fEOF && buff.IsEmpty()) break;
+
         FastTrim(buff);
         if(buff.IsEmpty() || buff.GetAt(0) == ';') continue;
 
@@ -1661,6 +1669,7 @@ if(sver <= 4)   style->scrAlignment = (style->scrAlignment&4) ? ((style->scrAlig
                 ret.m_eYCbCrRange = CSimpleTextSubtitle::YCbCrRange_PC;
             }
         }
+        if(fEOF) break;
     }
 //    ret.Sort();
     return(fRet);
@@ -1852,7 +1861,7 @@ static CStringW MPL22SSA(CStringW str, bool fUnicode, int CharSet)
 static bool OpenMPL2(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet)
 {
     CStringW buff;
-    while (file->ReadString(buff)) {
+    while(file->ReadString(buff)) {
         FastTrim(buff);
         if (buff.IsEmpty()) {
             continue;
@@ -1879,7 +1888,7 @@ static bool OpenRealText(CTextFile* file, CSimpleTextSubtitle& ret, int CharSet)
     std::wstring szFile;
     CStringW buff;
 
-    while (file->ReadString(buff)) {
+    while(file->ReadString(buff)) {
         FastTrim(buff);
         if (buff.IsEmpty()) {
             continue;