fee81a6d1abc6276c51115b6ef28475609b83540
Masaiki <mydarer@gmail.com>
2022-08-30 23:17:15 +0800

    fix: time shift when playing external sup files

--- src/subtitles/hdmv_subtitle_provider.cpp
+++ src/subtitles/hdmv_subtitle_provider.cpp
@@ -484,8 +484,8 @@ DWORD SupFileSubtitleProvider::ThreadProc()
     while (sub.GetPosition() < (sub.GetLength() - 10)) {
         sync = (WORD)ReadByte(&sub, 2);
         if (sync == 'PG') {
-            rtStart = UINT64(ReadByte(&sub, 4) * (1000 / 9));
-            rtStop = UINT64(ReadByte(&sub, 4) * (1000 / 9));
+            rtStart = UINT64(ReadByte(&sub, 4) * 1000 / 9);
+            rtStop = UINT64(ReadByte(&sub, 4) * 1000 / 9);
             sub.Seek(1, CFile::current); // Segment type
             size = ReadByte(&sub, 2) + 3;    // Segment size
             sub.Seek(-3, CFile::current);
